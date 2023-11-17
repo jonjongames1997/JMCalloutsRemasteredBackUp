2da9ff057cf2fadd50d24ac5b2de6e42a9449ab8
Jonathan Morrison <87885521+jonjongames1997@users.noreply.github.com>
2023-11-17 10:47:42 -0500

    Update GangRivalryShootout.cs

--- Callouts/GangRivalryShootout.cs
+++ Callouts/GangRivalryShootout.cs
@@ -3,10 +3,23 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Rage;
+using CalloutInterfaceAPI;
+using LSPD_First_Response.Mod.API;
+using LSPD_First_Response.Mod.Callouts;
+using System.Drawing;
+using System.Windows.Forms;
+using JMCalloutsRemastered.Stuff;
+using LSPD_First_Response.Engine.Scripting.Entities;
+using LSPD_First_Response.Engine.Scripting;
 
 namespace JMCalloutsRemastered.Callouts
 {
-    class GangRivalryShootout
+
+    [CalloutInterface("Gang Rivalry Shootout", CalloutProbability.High, "Reports of a gang shootout", "Code 99", "SWAT")]
+
+    public class GangRivalryShootout : Callout
     {
+
     }
 }
