ba339f60ae6e4878dede859ab498a2281eb65011
Jonathan Morrison <87885521+jonjongames1997@users.noreply.github.com>
2023-11-17 17:57:10 -0500

    Update MoneyTruckRobbery.cs

--- Callouts/MoneyTruckRobbery.cs
+++ Callouts/MoneyTruckRobbery.cs
@@ -155,9 +155,19 @@ namespace JMCalloutsRemastered.Callouts
 
         public override void End()
         {
-
-
+            if (blip1) blip1.Delete();
+            if (blip2) blip2.Delete();
+            if (blip3) blip3.Delete();
+            if (blip4) blip4.Delete();
+            if (Aggressor1) Aggressor1.Dismiss();
+            if (Aggressor2) Aggressor2.Dismiss();
+            if (Aggressor3) Aggressor3.Dismiss();
+            if (Aggressor4) Aggressor4.Dismiss();
+            Game.DisplayNotification("web_jonjongames", "web_jonjongames", "~w~JM Callouts Remastered", "~y~Reports of an Armed Money Truck Robbery", "~b~You: Dispatch, We are ~g~CODE 4~w~! Show me back 10-8!");
+            LSPD_First_Response.Mod.API.Functions.PlayScannerAudio("ATTENTION_THIS_IS_DISPATCH_HIGH ALL_UNITS_CODE4 NO_FURTHER_UNITS_REQUIRED");
             base.End();
+
+            Game.LogTrivial("JM Callouts Remastered: Money Truck Robbery is code 4");
         }
     }
 }
