fe4318f89ed7ca2154059dff3e812a6f210c1a2a
Jonathan Morrison <87885521+jonjongames1997@users.noreply.github.com>
2023-11-16 21:36:09 -0500

    Create DeadBody.cs

---
+++ Callouts/DeadBody.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Rage;
+using CalloutInterfaceAPI;
+using LSPD_First_Response.Mod.API;
+using LSPD_First_Response.Mod.Callouts;
+using System.Drawing;
+using System.Windows.Forms;
+using JMCalloutsRemastered.Stuff;
+
+namespace JMCalloutsRemastered.Callouts
+{
+
+    [CalloutInterface("Dead Bpdy", CalloutProbability.High, "Reports of a dead body", "Code 3", "LSPD")]
+
+    public class DeadBody : Callout
+    {
+
+    }
+}
