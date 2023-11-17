42318100f8c0b7ebf76b4cd3f56e2d54f38c4d42
Jonathan Morrison <87885521+jonjongames1997@users.noreply.github.com>
2023-11-17 12:55:41 -0500

    Create SolicitingDelPerroPier.cs

---
+++ Callouts/SolicitingDelPerroPier.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Rage;
+using CalloutInterfaceAPI;
+using LSPD_First_Response.Mod.API;
+using LSPD_First_Response.Mod.Callouts;
+using System.Drawing;
+using System.Windows.Forms;
+using JMCalloutsRemastered.Stuff;
+using LSPD_First_Response.Engine.Scripting.Entities;
+using LSPD_First_Response.Engine.Scripting;
+
+namespace JMCalloutsRemastered.Callouts
+{
+
+    [CalloutInterface("Soliciting - Del Perro Pier", CalloutProbability.Medium, "Reports of an individual asking people for money", "Code 2", "LSPD")]
+
+    public class SolicitingDelPerroPier : Callout
+    {
+
+    }
+}
