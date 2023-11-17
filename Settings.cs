151326e49c081f34726a81697b79a43670999107
Jonathan Morrison <87885521+jonjongames1997@users.noreply.github.com>
2023-11-17 12:53:05 -0500

    Update Settings.cs

--- Settings.cs
+++ Settings.cs
@@ -28,6 +28,7 @@ namespace JMCalloutsRemastered
         internal static bool SuspiciousPerson = true;
         internal static bool PersonWithAWeapon = true;
         internal static bool GangRivalryShootout = true;
+        internal static bool RocketMan = true;
         internal static bool ActiveAIBackup = true;
         internal static bool HelpMessages = true;
         internal static Keys EndCall = Keys.End;
@@ -61,6 +62,7 @@ namespace JMCalloutsRemastered
             Settings.SuspiciousPerson = ini.ReadBoolean("Callouts", "Suspicious Person", true);
             Settings.PersonWithAWeapon = ini.ReadBoolean("Callouts", "Person With A Weapon", true);
             Settings.GangRivalryShootout = ini.ReadBoolean("Callouts", "Gang Rivalry Shootout", true);
+            Settings.RocketMan = ini.ReadBoolean("Callots", "RocketMan", true);
             Settings.ActiveAIBackup = ini.ReadBoolean("General", "ActiveAIBackup", true);
             Settings.HelpMessages = ini.ReadBoolean("HelpMessages", "Help Messages", true);
             EndCall = ini.ReadEnum("Keys", "EndCall", Keys.End);
