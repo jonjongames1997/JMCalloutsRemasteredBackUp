3dafabb7d1133f90ae3391c659405da830ff773b
Jonathan Morrison <87885521+jonjongames1997@users.noreply.github.com>
2023-11-17 12:38:41 -0500

    Update RocketMan.cs

--- Callouts/RocketMan.cs
+++ Callouts/RocketMan.cs
@@ -66,9 +66,17 @@ namespace JMCalloutsRemastered.Callouts
 
         public override void OnCalloutNotAccepted()
         {
-
+            if (suspect) suspect.Delete();
+            if (blip) blip.Delete();
 
             base.OnCalloutNotAccepted();
+        }
+
+        public override void Process()
+        {
+
+
+            base.Process();
         }
     }
 }
