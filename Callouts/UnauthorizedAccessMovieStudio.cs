b952926174bd3a5021ae569cbd58dca52e0b56d8
Jonathan Morrison <87885521+jonjongames1997@users.noreply.github.com>
2023-11-16 20:54:56 -0500

    Create UnauthorizedAccessMovieStudio.cs

---
+++ Callouts/UnauthorizedAccessMovieStudio.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Rage;
+using CalloutInterfaceAPI;
+using LSPD_First_Response.Mod.API;
+using LSPD_First_Response.Mod.Callouts;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace JMCalloutsRemastered.Callouts
+{
+
+    [CalloutInterface("Unauthorized Access - Richard's Majestic", CalloutProbability.Medium, "Reports of a trespasser", "Code 2", "LSPD")]
+
+    public class UnauthorizedAccessMovieStudio : Callout
+    {
+
+    }
+}
