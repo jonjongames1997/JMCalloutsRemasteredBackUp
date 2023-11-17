3c99b05e7f1b46057aa47910fed6015594e421f5
Jonathan Morrison <87885521+jonjongames1997@users.noreply.github.com>
2023-11-16 21:43:08 -0500

    Update DeadBody.cs

--- Callouts/DeadBody.cs
+++ Callouts/DeadBody.cs
@@ -10,6 +10,7 @@ using LSPD_First_Response.Mod.Callouts;
 using System.Drawing;
 using System.Windows.Forms;
 using JMCalloutsRemastered.Stuff;
+using Rage.Native;
 
 namespace JMCalloutsRemastered.Callouts
 {
@@ -18,6 +19,33 @@ namespace JMCalloutsRemastered.Callouts
 
     public class DeadBody : Callout
     {
+        private Ped deadBody;
+        private Blip deadBlip;
 
+        public override bool OnBeforeCalloutDisplayed()
+        {
+            Vector3 spawnPoint = World.GetNextPositionOnStreet(Game.LocalPlayer.Character.Position.Around(1000f));
+
+            deadBody = new Ped(spawnPoint);
+            deadBody.IsPersistent = true;
+            deadBody.Kill();
+
+            NativeFunction.Natives.APPLY_PED_DAMAGE_PACK(deadBody, "BigHitByVehicle", 1f, 1f);
+
+            CalloutMessage = "Reports of a dead body";
+            CalloutPosition = spawnPoint;
+
+            LSPD_First_Response.Mod.API.Functions.PlayScannerAudio("ATTENTION_ALL_UNITS_02 WE_HAVE_01 CITIZENS_REPORT_04 CRIME_HIT_AND_RUN_03 IN_OR_ON_POSITION");
+            ShowCalloutAreaBlipBeforeAccepting(spawnPoint, 50);
+
+            return base.OnBeforeCalloutDisplayed();
+        }
+
+        public override bool OnCalloutAccepted()
+        {
+
+
+            return base.OnCalloutAccepted();
+        }
     }
 }
