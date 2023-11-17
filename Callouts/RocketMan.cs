e390de8c17bd07b82f27a90392545d665ba457b9
Jonathan Morrison <87885521+jonjongames1997@users.noreply.github.com>
2023-11-17 12:52:12 -0500

    Update RocketMan.cs

--- Callouts/RocketMan.cs
+++ Callouts/RocketMan.cs
@@ -117,9 +117,13 @@ namespace JMCalloutsRemastered.Callouts
 
         public override void End()
         {
-
-
+            if (suspect) suspect.Dismiss();
+            if (blip) blip.Delete();
+            Game.DisplayNotification("web_jonjongames", "web_jonjongames", "~w~JM Callouts Remastered", "~y~Reports of an Armed Individual with a Explosive Weapon", "~b~You: Dispatch, We are ~g~CODE 4~w~! Show me back 10-8!");
+            LSPD_First_Response.Mod.API.Functions.PlayScannerAudio("ATTENTION_THIS_IS_DISPATCH_HIGH ALL_UNITS_CODE4 NO_FURTHER_UNITS_REQUIRED");
             base.End();
+
+            Game.LogTrivial("[JM Callouts Remastered]: RocketMan callout is code 4!");
         }
     }
 }
