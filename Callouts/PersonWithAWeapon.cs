0e23304183cb4034aacbee49d70738c8ea22c610
Jonathan Morrison <87885521+jonjongames1997@users.noreply.github.com>
2023-11-16 22:52:03 -0500

    Create PersonWithAWeapon.cs

---
+++ Callouts/PersonWithAWeapon.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Rage;
+using CalloutInterfaceAPI;
+using LSPD_First_Response.Mod.API;
+using LSPD_First_Response.Mod.Callouts;
+using System.Drawing;
+using System.Windows.Forms;
+using JMCalloutsRemastered.Stuff;
+using LSPD_First_Response.Engine.Scripting.Entities;
+using LSPD_First_Response.Engine.Scripting;
+
+namespace JMCalloutsRemastered.Callouts
+{
+
+    [CalloutInterface("Person With A Weapon", CalloutProbability.High, "Reports of a individual with a weapon", "Code 3", "LSPD")]
+
+    public class PersonWithAWeapon : Callout
+    {
+
+    }
+}
