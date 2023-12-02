2da752609c17702276d35e797179533197be03d3
Jonathan Morrison <87885521+jonjongames1997@users.noreply.github.com>
2023-12-02 07:04:03 -0500

    Update TheCandyCaneWhacker.cs

--- Callouts/TheCandyCaneWhacker.cs
+++ Callouts/TheCandyCaneWhacker.cs
@@ -20,6 +20,23 @@ namespace JMCalloutsRemastered.Callouts
 
     public class TheCandyCaneWhacker : Callout
     {
+        private string[] wepList = new string[] { "WEAPON_CANDYCANE" };
+        private string[] pedList = new string[] { "ig_amandatownley", "a_m_m_afriamer_01", "ig_ashley", "u_m_y_babyd", "a_f_y_eastsa_03", "ig_jimmydisanto" };
+        private Ped suspect;
+        private Vector3 spawnpoint;
+        private Vector3 searcharea;
+        private Blip blip;
+        private LHandle pursuit;
+        private int scenario = 0;
+        private bool hasBegunAttacking = false;
+        private bool isArmed = false;
+        private bool hasPursuitBegun = false;
 
+        public override bool OnBeforeCalloutDisplayed()
+        {
+
+
+            return base.OnBeforeCalloutDisplayed();
+        }
     }
 }
