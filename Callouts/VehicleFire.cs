84f28afbd91af7ba049cfeccd573fddbf46c0374
Jonathan Morrison <87885521+jonjongames1997@users.noreply.github.com>
2023-12-04 17:45:14 -0500

    update

---
+++ Callouts/VehicleFire.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Rage;
+using LSPD_First_Response.Mod.API;
+using LSPD_First_Response.Mod.Callouts;
+using System.Drawing;
+using CalloutInterfaceAPI;
+using System.Windows.Forms;
+
+namespace JMCalloutsRemastered.Callouts
+{
+
+    [CalloutInterface("Vehicle Fire", CalloutProbability.Medium, "Citizen's report of a vehicle on fire", "Code 3", "SAHP")]
+
+    public class VehicleFire : Callout
+    {
+
+    }
+}
