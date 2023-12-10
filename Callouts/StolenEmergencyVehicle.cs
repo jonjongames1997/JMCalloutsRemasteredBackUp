24407b2c452996e1ea06b024270b75e4e4b69a2f
Jonathan Morrison <87885521+jonjongames1997@users.noreply.github.com>
2023-12-10 18:01:42 -0500

    Update StolenEmergencyVehicle.cs

--- Callouts/StolenEmergencyVehicle.cs
+++ Callouts/StolenEmergencyVehicle.cs
@@ -1,5 +1,14 @@
 ﻿using CalloutInterfaceAPI;
 using LSPD_First_Response.Mod.Callouts;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using JMCalloutsRemastered.Callouts;
+using JMCalloutsRemastered.Stuff;
+using LSPD_First_Response.Engine.Scripting;
+using LSPD_First_Response.Mod.API;
+using Rage;
 
 namespace JMCalloutsRemastered.Callouts
 {
@@ -8,7 +17,20 @@ namespace JMCalloutsRemastered.Callouts
 
     public class StolenEmergencyVehicle : Callout
     {
-        private string[] copVehicles = new string[] { "POLICE", "POLICE2", "POLICE3", "SHERIFF", "SHERIFF2", "POLICE4", "FBI", "FBI2", "AMBULANCE", "FIRETRUK", "POLICEB" };
+        private string[] emergencyVehicles = new string[] { "POLICE", "POLICE2", "POLICE3", "SHERIFF", "SHERIFF2", "POLICE4", "FBI", "FBI2", "AMBULANCE", "FIRETRUK", "POLICEB" };
+        private Vehicle emergencyVehicle;
+        private Ped suspect;
+        private Vector3 spawnpoint;
+        private Blip blip;
+        private LHandle pursuit;
+        private bool pursuitCreated = false;
+
+        public override bool OnBeforeCalloutDisplayed()
+        {
+
+
+            return base.OnBeforeCalloutDisplayed();
+        }
 
     }
 }
