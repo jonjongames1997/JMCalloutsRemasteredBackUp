722cac2e2d8b89bfe74072bce85883327f2a3c8a
Jonathan Morrison <87885521+jonjongames1997@users.noreply.github.com>
2023-12-10 16:59:46 -0500

    Create StolenEmergencyVehicle.cs

---
+++ Callouts/StolenEmergencyVehicle.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CalloutInterfaceAPI;
+using JMCalloutsRemastered.Callouts;
+using JMCalloutsRemastered.Stuff;
+using Rage;
+using LSPD_First_Response.Mod.Callouts;
+using LSPD_First_Response.Mod.API;
+using LSPD_First_Response.Engine.Scripting;
+
+namespace JMCalloutsRemastered.Callouts
+{
+
+    [CalloutInterface("Stolen Emergency Vehicle", CalloutProbability.Medium, "Reports of a stolen emergency vehicle", "CODE 3", "LSPD")]
+
+    public class StolenEmergencyVehicle : Callout
+    {
+
+    }
+}
