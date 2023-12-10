67c4c28fcc4424fd1da0ae74ed90c90e279d9b33
Jonathan Morrison <87885521+jonjongames1997@users.noreply.github.com>
2023-12-10 13:49:25 -0500

    Update ATMRobbery.cs

--- Callouts/ATMRobbery.cs
+++ Callouts/ATMRobbery.cs
@@ -20,6 +20,31 @@ namespace JMCalloutsRemastered.Callouts
     [CAPI.CalloutInterface("ATM Robbery", CalloutProbability.VeryHigh, "A citizen reported that the suspect might have a weapon", "Code 3", "LSPD")]
     public class ATMRobbery : Callout
     {
+        private string[] wepList = new string[] { "WEAPON_CROWBAR", "WEAPON_KNIFE", "WEAPON_CANDYCANE", "WEAPON_PISTOL", "WEAPON_TACTICALRIFLE" };
+        private string[] pedList = new string[] { "A_F_Y_HIPPIE_01", "HC_DRIVER", "HC_GUNMAN", "IG_ORTEGA", "IG_OLD_MAN2", "IG_MOLLY", "S_F_Y_MIGRANT_01", "S_F_M_MAID_01", "IG_TENNISCOACH", "IG_TONYA", "MP_F_COCAINE_01" };
+        private Vector3 spawnpoint;
+        private Vector3 searcharea;
+        public Ped suspect;
+        private Ped player => Game.LocalPlayer.Character;
+        public Blip suspectBlip;
+        public Blip searchAreaBlip;
 
+        private bool hasBegunAttacking = false;
+        private bool hasPursuitBegun = false;
+        private bool hasPursuitBeenCreated = false;
+        private bool calloutRunning = false;
+        private bool suspectBlipCreated = false;
+        private bool hasSuspectSurrendered = false;
+
+        private LHandle pursuit;
+
+        private int mainScenario;
+
+        public override bool OnBeforeCalloutDisplayed()
+        {
+
+
+            return base.OnBeforeCalloutDisplayed();
+        }
     }
 }
