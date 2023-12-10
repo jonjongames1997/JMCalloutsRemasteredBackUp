735fd9917dfdf118796ef4d31844b1ea5bcbcaf9
Jonathan Morrison <87885521+jonjongames1997@users.noreply.github.com>
2023-12-10 14:48:00 -0500

    Create LostPet.cs

---
+++ Callouts/LostPet.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LSPD_First_Response.Mod.API;
+using LSPD_First_Response.Mod.Callouts;
+using LSPD_First_Response.Engine.Scripting;
+using JMCalloutsRemastered.Stuff;
+using CalloutInterfaceAPI;
+
+namespace JMCalloutsRemastered.Callouts
+{
+
+    [CalloutInterface("Lost Pet", CalloutProbability.Medium, "A citizen's report of a lost animal", "Code 2", "LSPD")]
+
+    public class LostPet : Callout
+    {
+
+    }
+}
