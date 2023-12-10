c7fe4f7aab4df1ed51d94dd6907ab8697ae97fdf
Jonathan Morrison <87885521+jonjongames1997@users.noreply.github.com>
2023-12-10 18:06:01 -0500

    Update StolenEmergencyVehicle.cs

--- Callouts/StolenEmergencyVehicle.cs
+++ Callouts/StolenEmergencyVehicle.cs
@@ -27,7 +27,12 @@ namespace JMCalloutsRemastered.Callouts
 
         public override bool OnBeforeCalloutDisplayed()
         {
-
+            spawnpoint = World.GetNextPositionOnStreet(Game.LocalPlayer.Character.Position.Around(1000f));
+            ShowCalloutAreaBlipBeforeAccepting(spawnpoint, 100f);
+            CalloutInterfaceAPI.Functions.SendMessage(this, "Reports of a reckless driver in the area");
+            LSPD_First_Response.Mod.API.Functions.PlayScannerAudio("ATTENTION_ALL_UNITS_01 WE_HAVE_01 CRIME_SUSPECT_ON_THE_RUN_01 UNITS_RESPOND_CODE_03_02");
+            CalloutMessage = "Multiple reports of a reckless driver";
+            CalloutPosition = spawnpoint;
 
             return base.OnBeforeCalloutDisplayed();
         }
