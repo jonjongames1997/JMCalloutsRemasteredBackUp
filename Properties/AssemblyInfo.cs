bff8336ff6ce6ac99ab942edc5e59f18d5a5bcb8
Jonathan Morrison <87885521+jonjongames1997@users.noreply.github.com>
2023-12-14 19:17:37 -0500

    Update AssemblyInfo.cs

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -5,7 +5,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("JMCalloutsRemastered")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("LSPDFR Callout Pack for LSPDFR Police Mod")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("JonJonGamesStudiosInc")]
 [assembly: AssemblyProduct("JMCalloutsRemastered")]
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("3.9.3.1")]
-[assembly: AssemblyFileVersion("3.9.3.1")]
+[assembly: AssemblyVersion("3.9.3.2")]
+[assembly: AssemblyFileVersion("3.9.3.2")]
