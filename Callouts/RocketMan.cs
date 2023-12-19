6223fc7b4fde4c90b92c147ef39e2c1fb5684526
Jonathan Morrison <87885521+jonjongames1997@users.noreply.github.com>
2023-12-18 21:38:37 -0500

    Update RocketMan.cs

--- Callouts/RocketMan.cs
+++ Callouts/RocketMan.cs
@@ -30,7 +30,7 @@ namespace JMCalloutsRemastered.Callouts
             spawnpoint = World.GetNextPositionOnStreet(Game.LocalPlayer.Character.Position.Around(1000f));
             ShowCalloutAreaBlipBeforeAccepting(spawnpoint, 100f);
             CalloutInterfaceAPI.Functions.SendMessage(this, "A citizen's report of an individual with an explosive weapon.");
-            LSPD_First_Response.Mod.API.Functions.PlayScannerAudio("ROCKETMAN");
+            LSPD_First_Response.Mod.API.Functions.PlayScannerAudio("HERECOMESROCKETMAN");
             CalloutMessage = "Reports of an armed individual with an explosive weapon";
             CalloutPosition = spawnpoint;
 
@@ -81,7 +81,7 @@ namespace JMCalloutsRemastered.Callouts
                     {
                         suspect.KeepTasks = true;
                         suspect.Tasks.FightAgainst(Game.LocalPlayer.Character);
-                        LSPD_First_Response.Mod.API.Functions.PlayScannerAudio("ROCKETMAN");
+                        LSPD_First_Response.Mod.API.Functions.PlayScannerAudio("HERECOMESROCKETMAN");
                         suspect.Armor = 500;
                         hasBegunAttacking = true;
                         GameFiber.Wait(2000);
