d6af0b7f0bfc3392a6140fd44ea7f1d1419fc01f
Jonathan Morrison <87885521+jonjongames1997@users.noreply.github.com>
2023-12-27 19:14:39 -0500

    Create LockPickingSchool.cs

---
+++ Callouts/LockPickingSchool.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+using CalloutInterfaceAPI;
+using JMCalloutsRemastered.Callouts;
+using JMCalloutsRemastered.Stuff;
+using LSPD_First_Response.Mod.API;
+using LSPD_First_Response.Mod.Callouts;
+using LSPD_First_Response.Engine.Scripting;
+using LSPD_First_Response.Engine.Scripting.Entities;
+
+namespace JMCalloutsRemastered.Callouts
+{
+
+
+
+    public class LockPickingSchool : Callout
+    {
+
+    }
+}
