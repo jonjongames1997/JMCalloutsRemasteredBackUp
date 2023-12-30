4a6ef65c0aaea38a64589b51674887e1940e9abb
Jonathan Morrison <87885521+jonjongames1997@users.noreply.github.com>
2023-12-30 09:56:09 -0500

    Update Main.cs

--- Main.cs
+++ Main.cs
@@ -68,12 +68,10 @@ namespace JMCalloutsRemastered
             if (Settings.RocketMan) { Functions.RegisterCallout(typeof(RocketMan)); }
             if (Settings.SolicitingDelPerroPier) { Functions.RegisterCallout(typeof(SolicitingDelPerroPier)); }
             if (Settings.OfficerDown) { Functions.RegisterCallout(typeof(OfficerDown)); }
-            if (Settings.TheCandyCaneWhacker) { Functions.RegisterCallout(typeof(TheCandyCaneWhacker)); }
             if (Settings.VehicleFire) { Functions.RegisterCallout(typeof(VehicleFire)); }
             if (Settings.MonkeyWithAWeapon) { Functions.RegisterCallout(typeof(MonkeyWithAWeapon)); }
             if (Settings.WrecklessDriving) { Functions.RegisterCallout(typeof(WrecklessDriving)); }
             if (Settings.StolenEmergencyVehicle) { Functions.RegisterCallout(typeof(StolenEmergencyVehicle)); }
-            if (Settings.LockPickingSchool) { Functions.RegisterCallout(typeof(LockPickingSchool)); }
             Game.Console.Print("[LOG]: All callouts of the JMCalloutsRemastered.ini were loaded successfully.");
             Game.Console.Print();
             Game.Console.Print("================================================== JM Callouts Remastered ===================================================");
