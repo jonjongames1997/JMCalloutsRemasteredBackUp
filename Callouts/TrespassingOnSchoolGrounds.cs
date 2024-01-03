85ac30c9db1fb5767a33e1a602785a92144deadd
Jonathan Morrison <87885521+jonjongames1997@users.noreply.github.com>
2024-01-03 17:38:12 -0500

    Update TrespassingOnSchoolGrounds.cs

--- Callouts/TrespassingOnSchoolGrounds.cs
+++ Callouts/TrespassingOnSchoolGrounds.cs
@@ -45,9 +45,34 @@ namespace JMCalloutsRemastered.Callouts
 
         public override bool OnCalloutAccepted()
         {
+            Game.LogTrivial("[JM Callouts Remastered Log]: Trespassing On School Grounds callout accepted!");
+            Game.DisplayNotification("web_jonjongames", "web_jonjongames", "~w~JM Callouts Remastered", "~w~Trespassing On School Property", "~b~Dispatch~w~: Suspect has been spotted! Respond ~r~Code 2~w~.");
 
+            suspect = new Ped(pedList[new Random().Next((int)pedList.Length)], spawnpoint, 0f);
+            suspect.IsPersistent = true;
+            suspect.BlockPermanentEvents = true;
+            suspect.Inventory.GiveNewWeapon("WEAPON_UNARMED", 500, true);
+
+            blip = suspect.AttachBlip();
+            blip.Color = System.Drawing.Color.Orange;
+            blip.IsRouteEnabled = true;
+            blip.Alpha = 0.5f;
+
+            if (suspect.IsMale)
+                malefemale = "Sir";
+            else
+                malefemale = "Ma'am";
+
+            counter = 0;
 
             return base.OnCalloutAccepted();
+        }
+
+        public override void OnCalloutNotAccepted()
+        {
+
+
+            base.OnCalloutNotAccepted();
         }
     }
 }
