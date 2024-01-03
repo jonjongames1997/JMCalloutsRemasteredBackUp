489efc71f1486d30d662b2a54a708d78f39af9ef
Jonathan Morrison <87885521+jonjongames1997@users.noreply.github.com>
2024-01-03 17:27:13 -0500

    Update TrespassingOnSchoolGrounds.cs

--- Callouts/TrespassingOnSchoolGrounds.cs
+++ Callouts/TrespassingOnSchoolGrounds.cs
@@ -24,20 +24,29 @@ namespace JMCalloutsRemastered.Callouts
         private string[] pedList = new string[] { "PLAYER_TWO", "PLAYER_ZERO", "PLAYER_ONE", "IG_AMANDATOWNLEY", "S_F_Y_BARTENDER_01", "IG_BEVERLY", "U_F_Y_BIKERCHIC", "G_M_M_CHEMWORK_01", "MP_F_FREEMODE_01", "HC_HACKER", "A_F_Y_RURMETH_01", "MP_F_COCAINE_01" };
         private string[] wepList = new string[] { "WEAPON_PISTOL", "WEAPON_STUNGUN", "WEAPON_DAGGER", "WEAPON_KNIFE", "WEAPON_WRENCH", "WEAPON_RAYPISTOL", "WEAPON_AUTOSHOTGUN", "WEAPON_ASSAULTRIFLE", "WEAPON_CARBINERIFLE" };
         private Vector3 spawnpoint;
-        private Vector3 searchArea;
         private Blip blip;
         private Ped suspect;
-        private LHandle pursuit;
-        private int scenario = 0;
-        private bool hasBegunAttacking = false;
-        private bool isArmed = false;
-        private bool hasPursuitBegun = false;
+        private int counter;
+        private float heading;
+        private string malefemale;
 
         public override bool OnBeforeCalloutDisplayed()
         {
-
+            spawnpoint = new Vector3(-1602.71f, 206.43f, 59.28f);
+            heading = 100.56f;
+            ShowCalloutAreaBlipBeforeAccepting(spawnpoint, 100f);
+            CalloutInterfaceAPI.Functions.SendMessage(this, "An unknown individual trespassing on school property");
+            CalloutMessage = "Reports of an unknown person trespassing";
+            CalloutPosition = spawnpoint;
 
             return base.OnBeforeCalloutDisplayed();
+        }
+
+        public override bool OnCalloutAccepted()
+        {
+
+
+            return base.OnCalloutAccepted();
         }
     }
 }
