83989f948930169e9ba44fd768247a57918dbf0b
Jonathan Morrison <87885521+jonjongames1997@users.noreply.github.com>
2024-01-26 10:35:06 -0500

    Update PornStudioDispute.cs

--- Callouts/PornStudioDispute.cs
+++ Callouts/PornStudioDispute.cs
@@ -1,12 +1,22 @@
-﻿using System;
+﻿using CalloutInterfaceAPI;
+using LSPD_First_Response.Mod.Callouts;
+using Rage;
+using System;
+using System.Drawing;
+using System.Collections;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using JMCalloutsRemastered;
+using JMCalloutsRemastered.Stuff;
+using LSPD_First_Response.Engine.Scripting.Entities;
+using LSPD_First_Response.Engine.Scripting;
 
 namespace JMCalloutsRemastered.Callouts
 {
-    class PornStudioDispute
+
+    [CalloutInterface("Porn Studio Dispute - Patron-Only", CalloutProbability.Medium, "A person causing a scene", "Code 2", "LSPD")]
+
+    public class PornStudioDispute : Callout
     {
+
     }
 }
