6499f55f1388374c26413a6762cd43b607fe7d2d
Jonathan Morrison <87885521+jonjongames1997@users.noreply.github.com>
2024-01-27 10:36:41 -0500

    Create PersonOnTheHighway.cs

---
+++ Callouts/PersonOnTheHighway.cs
@@ -0,0 +1,22 @@
+﻿using CalloutInterfaceAPI;
+using LSPD_First_Response.Mod.Callouts;
+using Rage;
+using System;
+using System.Drawing;
+using System.Collections;
+using System.Collections.Generic;
+using JMCalloutsRemastered;
+using JMCalloutsRemastered.Stuff;
+using LSPD_First_Response.Engine.Scripting.Entities;
+using LSPD_First_Response.Engine.Scripting;
+
+namespace JMCalloutsRemastered.Callouts
+{
+
+    [CalloutInterface("Person On The Highway", CalloutProbability.Medium, "A citizen's report of an individual on the freeway", "Code 2", "SAHP")]
+
+    public class PersonOnTheHighway : Callout
+    {
+
+    }
+}
