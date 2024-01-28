360e2cf4fa03b497f7a30edfc1b4e4f461403cac
Jonathan Morrison <87885521+jonjongames1997@users.noreply.github.com>
2024-01-27 21:44:22 -0500

    Update CodeKaren.cs

--- Callouts/CodeKaren.cs
+++ Callouts/CodeKaren.cs
@@ -49,6 +49,7 @@ namespace JMCalloutsRemastered.Callouts
         {
             Game.LogTrivial("[JM Callouts Remastered Log]: Code Karen callout accepted!");
             Game.DisplayNotification("web_jonjongames", "web_jonjongames", "~w~JM Callouts Remastered", "~w~Code Karan", "~b~Dispatch: ~w~Suspect spotted. Respond ~Code 2.");
+            Game.DisplayHelp("Press ~y~END~w~ at anytime to end the callout", false);
 
             Suspect = new Ped(Spawnpoint);
             Suspect.IsPersistent = true;
