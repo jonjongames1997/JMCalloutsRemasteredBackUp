5605351e24ac28c1a46c961400541ceeecb014b5
Jonathan Morrison <87885521+jonjongames1997@users.noreply.github.com>
2024-01-27 21:50:27 -0500

    Update Settings.cs

--- Settings.cs
+++ Settings.cs
@@ -39,6 +39,7 @@ namespace JMCalloutsRemastered
         internal static bool WrecklessDriving = true;
         internal static bool StolenEmergencyVehicle = true;
         internal static bool TrespassingOnSchoolGrounds = true;
+        internal static bool PersonOnTheHighway = true;
         internal static bool HelpMessages = true;
         internal static Keys EndCall = Keys.End;
         internal static Keys Dialog = Keys.E;
@@ -85,6 +86,7 @@ namespace JMCalloutsRemastered
             Settings.WrecklessDriving = ini.ReadBoolean("Callouts", "RecklessDriving", true);
             Settings.StolenEmergencyVehicle = ini.ReadBoolean("Callouts", "StolenEmergencyVehicle", true);
             Settings.TrespassingOnSchoolGrounds = ini.ReadBoolean("Callouts", "TrespassingOnSchoolGrounds", true);
+            Settings.PersonOnTheHighway = ini.ReadBoolean("Callouts", "PersonOnTheHighway", true);
             Settings.HelpMessages = ini.ReadBoolean("HelpMessages", "Help Messages", true);
             EndCall = ini.ReadEnum("Keys", "EndCall", Keys.End);
             Dialog = ini.ReadEnum("Keys", "Dialog", Keys.E);
