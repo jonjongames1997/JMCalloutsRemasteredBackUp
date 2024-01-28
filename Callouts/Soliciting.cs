867598de3e0406bb6f22431b10dd8435c9dc6a71
Jonathan Morrison <87885521+jonjongames1997@users.noreply.github.com>
2024-01-27 21:54:58 -0500

    Update Soliciting.cs

--- Callouts/Soliciting.cs
+++ Callouts/Soliciting.cs
@@ -49,6 +49,7 @@ namespace JMCalloutsRemastered.Callouts
         {
             Game.LogTrivial("[JM Callouts Remastered Log]: Soliciting Callout accaepted!");
             Game.DisplayNotification("web_jonjongames", "web_jonjongames", "~w~JM Callouts Remastered", "~w~Soliciting", "~b~Dispatch:~w~ Suspect has been spotted. Respond ~r~Code 2.");
+            Game.DisplayHelp("Press ~y~END~w~ at anytime to end the callout", false);
 
             Suspect = new Ped(Spawnpoint);
             Suspect.IsPersistent = true;
@@ -101,8 +102,6 @@ namespace JMCalloutsRemastered.Callouts
 
             if (Game.LocalPlayer.Character.IsDead) End();
             if (Game.IsKeyDown(Settings.EndCall)) End();
-            if (Suspect && Suspect.IsDead) End();
-            if (Suspect && LSPD_First_Response.Mod.API.Functions.IsPedArrested(Suspect)) End();
         }
 
         public override void End()
