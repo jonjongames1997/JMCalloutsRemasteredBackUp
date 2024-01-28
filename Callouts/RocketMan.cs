e3f7d17e81eb889f6d408c8eceddb7f9da2606ee
Jonathan Morrison <87885521+jonjongames1997@users.noreply.github.com>
2024-01-27 21:56:04 -0500

    Update RocketMan.cs

--- Callouts/RocketMan.cs
+++ Callouts/RocketMan.cs
@@ -1,9 +1,16 @@
 ﻿using CalloutInterfaceAPI;
-using LSPD_First_Response.Mod.API;
 using LSPD_First_Response.Mod.Callouts;
 using Rage;
 using System;
 using System.Drawing;
+using System.Collections;
+using System.Collections.Generic;
+using JMCalloutsRemastered;
+using JMCalloutsRemastered.Stuff;
+using LSPD_First_Response.Engine.Scripting.Entities;
+using LSPD_First_Response.Engine.Scripting;
+using JMCalloutsRemastered.Callouts;
+using LSPD_First_Response.Mod.API;
 
 namespace JMCalloutsRemastered.Callouts
 {
