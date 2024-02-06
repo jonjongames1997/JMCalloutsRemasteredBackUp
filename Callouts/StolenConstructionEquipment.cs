b2b0eb747ad2d243a3ba16edead33978908525fb
Jonathan Morrison <87885521+jonjongames1997@users.noreply.github.com>
2024-02-05 23:21:30 -0500

    Update StolenConstructionEquipment.cs

--- Callouts/StolenConstructionEquipment.cs
+++ Callouts/StolenConstructionEquipment.cs
@@ -1,12 +1,26 @@
-﻿using System;
+﻿using CalloutInterfaceAPI;
+using LSPD_First_Response.Mod.Callouts;
+using Rage;
+using System;
+using System.Drawing;
+using System.Collections;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using JMCalloutsRemastered;
+using JMCalloutsRemastered.Stuff;
+using LSPD_First_Response.Engine.Scripting.Entities;
+using LSPD_First_Response.Engine.Scripting;
+using JMCalloutsRemastered.Callouts;
+using LSPD_First_Response.Mod.API;
+using System.Threading;
 
 namespace JMCalloutsRemastered.Callouts
 {
-    class StolenConstructionEquipment
+
+    [CalloutInterface("Stolen Construction Equipment", CalloutProbability.Medium, "Reports of stolen construction equipment.", "Code 3", "LSPD")]
+
+
+    public class StolenConstructionEquipment : Callout
     {
+
     }
 }
