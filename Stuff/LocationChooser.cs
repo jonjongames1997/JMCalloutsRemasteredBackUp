e032194233cfb9e051b5acac5977a8be401c4e4b
Jonathan Morrison <87885521+jonjongames1997@users.noreply.github.com>
2024-02-06 08:25:45 -0500

    Update LocationChooser.cs

--- Stuff/LocationChooser.cs
+++ Stuff/LocationChooser.cs
@@ -6,29 +6,29 @@ namespace JMCalloutsRemastered.Stuff
     public static class LocationChooser
     {
 
-        public static Vector3 chooseNearestLocation(List<Vector3> list)
+        internal static Vector3 ChooseNearestLocation(List<Vector3> list)
         {
             Vector3 closestLocation = list[0];
-            float closestDistance = Vector3.Distance(Game.LocalPlayer.Character.Position, list[0]);
+            float closestDistance = Vector3.Distance(MainPlayer.Position, list[0]);
             for (int i = 1; i < list.Count; i++)
             {
-                if (Vector3.Distance(Game.LocalPlayer.Character.Position, list[i]) <= closestDistance)
+                if (Vector3.Distance(MainPlayer.Position, list[i]) <= closestDistance)
                 {
-                    closestDistance = Vector3.Distance(Game.LocalPlayer.Character.Position, list[i]);
+                    closestDistance = Vector3.Distance(MainPlayer.Position, list[i]);
                     closestLocation = list[i];
                 }
             }
             return closestLocation;
         }
-        public static int nearestLocationIndex(List<Vector3> list)
+        internal static int NearestLocationIndex(List<Vector3> list)
         {
             int closestLocationIndex = 0;
-            float closestDistance = Vector3.Distance(Game.LocalPlayer.Character.Position, list[0]);
+            float closestDistance = Vector3.Distance(MainPlayer.Position, list[0]);
             for (int i = 1; i < list.Count; i++)
             {
-                if (Vector3.Distance(Game.LocalPlayer.Character.Position, list[i]) <= closestDistance)
+                if (Vector3.Distance(MainPlayer.Position, list[i]) <= closestDistance)
                 {
-                    closestDistance = Vector3.Distance(Game.LocalPlayer.Character.Position, list[i]);
+                    closestDistance = Vector3.Distance(MainPlayer.Position, list[i]);
                     closestLocationIndex = i;
                 }
             }
