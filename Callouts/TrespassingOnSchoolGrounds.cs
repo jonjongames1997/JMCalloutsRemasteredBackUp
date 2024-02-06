0c9b0492343ea9edcfb3498299660e5d6ec3a11e
Jonathan Morrison <87885521+jonjongames1997@users.noreply.github.com>
2024-02-06 09:13:10 -0500

    Update TrespassingOnSchoolGrounds.cs

--- Callouts/TrespassingOnSchoolGrounds.cs
+++ Callouts/TrespassingOnSchoolGrounds.cs
@@ -1,18 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using LSPD_First_Response.Engine.Scripting;
-using LSPD_First_Response.Engine.Scripting.Entities;
-using LSPD_First_Response.Mod.Callouts;
-using CalloutInterfaceAPI;
-using LSPD_First_Response.Mod.API;
-using JMCalloutsRemastered.Stuff;
-using JMCalloutsRemastered.Callouts;
-using Rage;
-using Rage.Native;
-using System.Drawing;
+﻿using CalloutInterfaceAPI;
 
 namespace JMCalloutsRemastered.Callouts
 {
@@ -21,18 +7,18 @@ namespace JMCalloutsRemastered.Callouts
 
     public class TrespassingOnSchoolGrounds : Callout
     {
-        private string[] pedList = new string[] { "PLAYER_TWO", "PLAYER_ZERO", "PLAYER_ONE", "IG_AMANDATOWNLEY", "S_F_Y_BARTENDER_01", "IG_BEVERLY", "U_F_Y_BIKERCHIC", "G_M_M_CHEMWORK_01", "MP_F_FREEMODE_01", "HC_HACKER", "A_F_Y_RURMETH_01", "MP_F_COCAINE_01" };
-        private string[] wepList = new string[] { "WEAPON_PISTOL", "WEAPON_STUNGUN", "WEAPON_DAGGER", "WEAPON_KNIFE", "WEAPON_WRENCH", "WEAPON_RAYPISTOL", "WEAPON_AUTOSHOTGUN", "WEAPON_ASSAULTRIFLE", "WEAPON_CARBINERIFLE" };
-        private Vector3 spawnpoint;
-        private Blip blip;
-        private Ped suspect;
-        private int counter;
-        private float heading;
-        private string malefemale;
+        private static readonly string[] pedList = new string[] { "PLAYER_TWO", "PLAYER_ZERO", "PLAYER_ONE", "IG_AMANDATOWNLEY", "S_F_Y_BARTENDER_01", "IG_BEVERLY", "U_F_Y_BIKERCHIC", "G_M_M_CHEMWORK_01", "MP_F_FREEMODE_01", "HC_HACKER", "A_F_Y_RURMETH_01", "MP_F_COCAINE_01" };
+        private static readonly string[] wepList = new string[] { "WEAPON_PISTOL", "WEAPON_STUNGUN", "WEAPON_DAGGER", "WEAPON_KNIFE", "WEAPON_WRENCH", "WEAPON_RAYPISTOL", "WEAPON_AUTOSHOTGUN", "WEAPON_ASSAULTRIFLE", "WEAPON_CARBINERIFLE" };
+        private static Vector3 spawnpoint;
+        private static Blip blip;
+        private static Ped suspect;
+        private static int counter;
+        private static float heading;
+        private static string malefemale;
 
         public override bool OnBeforeCalloutDisplayed()
         {
-            spawnpoint = new Vector3(-1602.71f, 206.43f, 59.28f);
+            spawnpoint = new(-1602.71f, 206.43f, 59.28f);
             heading = 100.56f;
             ShowCalloutAreaBlipBeforeAccepting(spawnpoint, 100f);
             CalloutInterfaceAPI.Functions.SendMessage(this, "An unknown individual trespassing on school property");
