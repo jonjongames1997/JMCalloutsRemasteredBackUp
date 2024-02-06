b20f52b071e475ef54946aac9ef6d96ce3fa6d8b
Jonathan Morrison <87885521+jonjongames1997@users.noreply.github.com>
2024-02-06 08:29:11 -0500

    Update 911HangUp.cs

--- Callouts/911HangUp.cs
+++ Callouts/911HangUp.cs
@@ -21,11 +21,11 @@ namespace JMCalloutsRemastered.Callouts
             Random random = new Random();
             List<Vector3> list = new List<Vector3>
             {
-                new Vector3(1082.087f, -346.2961f, 67.1872f), // Mirror Park near Horny's //
-                new Vector3(-1294.99f, -1316.46f, 4.69f), // OCRP Postal 305/Vespucci Beach //
-                new Vector3(-1281.43f, -1139.24f, 6.47f), // Bean Machine in Vespucci Beach
-                new Vector3(-1335.80f, -929.51f, 11.75f), // Motel Near Rob's Liquors
-                new Vector3(-232.05f, -2055.64f, 27.62f), // Maze Bank Arena Parking 
+                new (1082.087f, -346.2961f, 67.1872f), // Mirror Park near Horny's //
+                new (-1294.99f, -1316.46f, 4.69f), // OCRP Postal 305/Vespucci Beach //
+                new (-1281.43f, -1139.24f, 6.47f), // Bean Machine in Vespucci Beach
+                new (-1335.80f, -929.51f, 11.75f), // Motel Near Rob's Liquors
+                new (-232.05f, -2055.64f, 27.62f), // Maze Bank Arena Parking 
             };
             Spawnpoint = LocationChooser.ChooseNearestLocation(list);
             ShowCalloutAreaBlipBeforeAccepting(Spawnpoint, 100f);
