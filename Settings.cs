180524e4e8998bde4afdad29332c08b165b85e67
Jonathan Morrison <87885521+jonjongames1997@users.noreply.github.com>
2024-02-17 14:19:23 -0500

    Update Settings.cs

--- Settings.cs
+++ Settings.cs
@@ -26,6 +26,7 @@ namespace JMCalloutsRemastered
         internal static bool TrespassingOnSchoolGrounds = true;
         internal static bool PersonOnTheHighway = true;
         internal static bool StolenConstructionEquipment = true;
+        internal static bool DomesticDisturbance = true;
         internal static bool HelpMessages = true;
         internal static Keys EndCall = Keys.End;
         internal static Keys Dialog = Keys.E;
@@ -59,10 +60,11 @@ namespace JMCalloutsRemastered
             Settings.TrespassingOnSchoolGrounds = ini.ReadBoolean("Callouts", "TrespassingOnSchoolGrounds", true);
             Settings.PersonOnTheHighway = ini.ReadBoolean("Callouts", "PersonOnTheHighway", true);
             Settings.StolenConstructionEquipment = ini.ReadBoolean("Callouts", "StolenConstructionEquipment", true);
+            Settings.DomesticDisturbance = ini.ReadBoolean("Callouts", "DomesticDisturbance", true);
             Settings.HelpMessages = ini.ReadBoolean("HelpMessages", "Help Messages", true);
             EndCall = ini.ReadEnum("Keys", "EndCall", Keys.End);
             Dialog = ini.ReadEnum("Keys", "Dialog", Keys.E);
         }
-        public static readonly string PluginVersion = "4.0.0";
+        public static readonly string PluginVersion = "4.1.0";
     }
 }
