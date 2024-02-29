b8caacaac665c88025307dae3b685f4f34e56f70
Jonathan Morrison <87885521+jonjongames1997@users.noreply.github.com>
2024-02-28 21:14:15 -0500

    Update Main.cs

--- Main.cs
+++ Main.cs
@@ -1,4 +1,5 @@
-﻿using System.Reflection;
+﻿using System.Linq;
+using System.Reflection;
 using JMCalloutsRemastered.Callouts;
 
 [assembly: Rage.Attributes.Plugin("JMCalloutsRemastered", Description = "LSPDFR Callout Pack", Author = "OfficerMorrison")]
@@ -6,6 +7,10 @@ namespace JMCalloutsRemastered
 {
     public class Main : Plugin
     {
+        public static bool CalloutInterface;
+        public static bool StopThePed;
+        public static bool UltimateBackup;
+
         public override void Finally() { }
 
         public override void Initialize()
@@ -27,12 +32,13 @@ namespace JMCalloutsRemastered
                     Game.Console.Print("[VERSION]: Detected Version: " + Assembly.GetExecutingAssembly().GetName().Version.ToString());
                     Game.Console.Print("[LOG]: Checking for a new JM Callouts Remastered version...");
                     Game.Console.Print();
+                    Game.Console.Print("[LOG]: Checking to see if user has Callout Interface 1.4.1, Stop The Ped, and Ultimate Backup Installed...");
                     Game.Console.Print("=============================================== JM Callouts Remastered by OfficerMorrison ================================================");
                     Game.Console.Print();
 
 
                     Game.DisplayNotification("web_jonjongames", "web_jonjongames", "JM Callouts Remastered", "~g~v" + Assembly.GetExecutingAssembly().GetName().Version.ToString() + " ~r~by OfficerMorrison", "~b~successfully loaded!");
-                    Game.DisplayNotification("If you have InteriorsV mod installed, you need to disable Story Mode Compatibility. The 'Lost Individual' callout will break if not Disabled.");
+                    Game.DisplayNotification("I am ~y~NO LONGER SUPPORTING Old versions of JM Callouts Remasterd~w~. Delete any old versions you have and update to ~g~Latest~w~ build. Thank you! - JJG.");
 
                     VersionChecker.VersionChecker.IsUpdateAvailable();
                     GameFiber.Wait(300);
@@ -44,6 +50,36 @@ namespace JMCalloutsRemastered
             Game.Console.Print();
             Game.Console.Print("================================================== JM Callouts Remastered ===================================================");
             Game.Console.Print();
+            if(Functions.GetAllUserPlugins().ToList().Any(a => a != null && a.FullName.Contains("CalloutInterface")) == true)
+            {
+                Game.LogTrivial("User does have Callout Interface 1.4.1 installed. Starting integration.....");
+                CalloutInterface = true;
+            }
+            else
+            {
+                Game.LogTrivial("User does not have Callout Interface 1.4.1 Installed. Stopping integration....");
+                CalloutInterface = false;
+            }
+            if(Functions.GetAllUserPlugins().ToList().Any(a => a != null && a.FullName.Contains("StopThePed")) == true)
+            {
+                Game.LogTrivial("User does have Stop The Ped installed. Starting integration......");
+                StopThePed = true;
+            }
+            else
+            {
+                Game.LogTrivial("User does not have Stop The Ped installed. Stopping integration.......");
+                StopThePed = false;
+            }
+            if(Functions.GetAllUserPlugins().ToList().Any(a => a != null && a.FullName.Contains("UltimateBackup")) == true)
+            {
+                Game.LogTrivial("User does have Ultimate Backup installed. Starting integration......");
+                UltimateBackup = true;
+            }
+            else
+            {
+                Game.LogTrivial("User does not have Ultimate Backup installed. Stopping integration......");
+                UltimateBackup = false;
+            }
             if (Settings._911HangUp) { Functions.RegisterCallout(typeof(_911HangUp)); }
             if (Settings.CodeKaren) { Functions.RegisterCallout(typeof(CodeKaren)); }
             if (Settings.IllegalCampfireOnPublicBeach) { Functions.RegisterCallout(typeof(IllegalCampfireOnPublicBeach)); }
@@ -67,6 +103,8 @@ namespace JMCalloutsRemastered
             if (Settings.PersonOnTheHighway) { Functions.RegisterCallout(typeof(PersonOnTheHighway)); }
             if (Settings.StolenConstructionEquipment) { Functions.RegisterCallout(typeof(StolenConstructionEquipment)); }
             if (Settings.DomesticDisturbance) { Functions.RegisterCallout(typeof(DomesticDisturbance)); }
+            if (Settings.StolenMilitaryEquipment) { Functions.RegisterCallout(typeof(StolenMilitaryEquipment)); }
+            if (Settings.MovieHopping) { Functions.RegisterCallout(typeof(MovieHopping)); }
             Game.Console.Print("[LOG]: All callouts of the JMCalloutsRemastered.ini were loaded successfully.");
             Game.Console.Print();
             Game.Console.Print("================================================== JM Callouts Remastered ===================================================");
