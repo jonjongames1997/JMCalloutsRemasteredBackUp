9a20cea67aeae928259fb4c0e24a3350337214c2
Jonathan Morrison <87885521+jonjongames1997@users.noreply.github.com>
2024-04-06 22:00:42 -0400

    Update DomesticDisturbance.cs

--- Callouts/DomesticDisturbance.cs
+++ Callouts/DomesticDisturbance.cs
@@ -125,9 +125,7 @@ namespace JMCalloutsRemastered.Callouts
 
         public override void End()
         {
-            if (victim) victim.Dismiss();
             if (vicBlip) vicBlip.Delete();
-            if (suspect) suspect.Dismiss();
             if (susBlip) susBlip.Delete();
             Game.DisplayNotification("web_jonjongames", "web_jonjongames", "~w~JM Callouts Remastered", "~w~Domestic Disturbance", "~b~You~w~: Dispatch, we are ~g~Code 4~w~. Show me back 10-8.");
             LSPD_First_Response.Mod.API.Functions.PlayScannerAudio("ATTENTION_THIS_IS_DISPATCH_HIGH ALL_UNITS_CODE4 NO_FURTHER_UNITS_REQUIRED");
