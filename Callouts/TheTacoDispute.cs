e8c99e9902d7a68029f1860488b2f6399c820446
Jonathan Morrison <87885521+jonjongames1997@users.noreply.github.com>
2024-04-24 23:37:10 -0400

    Create TheTacoDispute.cs

---
+++ Callouts/TheTacoDispute.cs
@@ -0,0 +1,12 @@
+﻿using CalloutInterfaceAPI;
+
+namespace JMCalloutsRemastered.Callouts
+{
+
+
+    [CalloutInterface("The Taco Dispute", CalloutProbability.Medium, "A taco business requesting police assistance", "Code 2", "LSPD")]
+    public class TheTacoDispute : Callout
+    {
+
+    }
+}
