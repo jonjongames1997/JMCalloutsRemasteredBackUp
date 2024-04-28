c27a7f17680c5e826239166a7a3f304838ff3c43
Jonathan Morrison <87885521+jonjongames1997@users.noreply.github.com>
2024-04-28 19:59:40 -0400

    Update PeepingTom.cs

--- Callouts/PeepingTom.cs
+++ Callouts/PeepingTom.cs
@@ -24,6 +24,8 @@ namespace JMCalloutsRemastered.Callouts
                 new(-1623.53f, -362.89f, 46.40f),
                 new(-1532.85f, 27.17f, 56.82f),
                 new(-1713.22f, 386.81f, 89.73f),
+                new(),
+                new(),
             };
             spawnpoint = LocationChooser.ChooseNearestLocation(list);
             ShowCalloutAreaBlipBeforeAccepting(spawnpoint, 100f);
