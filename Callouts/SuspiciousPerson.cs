012853f823ba4670be1f536b19b7acf29aa968ef
Jonathan Morrison <87885521+jonjongames1997@users.noreply.github.com>
2024-05-03 18:28:21 -0400

    Update SuspiciousPerson.cs

--- Callouts/SuspiciousPerson.cs
+++ Callouts/SuspiciousPerson.cs
@@ -36,7 +36,7 @@ namespace JMCalloutsRemastered.Callouts
             suspect = new Ped(pedList[new Random().Next((int)pedList.Length)], spawnpoint, 0f);
             suspect.IsPersistent = true;
             suspect.BlockPermanentEvents = true;
-            suspect.Inventory.GiveNewWeapon("WEAPON_UNARMED", 500, true);
+            suspect.Inventory.GiveNewWeapon("WEAPON_UNARMED", 5000, true);
 
             susBlip = suspect.AttachBlip();
             susBlip.Color = System.Drawing.Color.CornflowerBlue;
@@ -64,7 +64,76 @@ namespace JMCalloutsRemastered.Callouts
         {
             base.Process();
 
+            if(MainPlayer.DistanceTo(suspect) <= 10f)
+            {
+                Game.DisplayHelp("Press ~y~E~w~ to talk to Suspect. ~y~Approach with caution~w~.", false);
 
+                if (Game.IsKeyDown(System.Windows.Forms.Keys.E))
+                {
+                    counter++;
+
+                    if(counter == 1)
+                    {
+                        suspect.Face(MainPlayer);
+                        Game.DisplaySubtitle("~b~You~w~: Police! Stop right there, " + malefemale + ". Better not reach for anything.");
+                    }
+                    if(counter == 2)
+                    {
+                        suspect.Tasks.PutHandsUp(500, MainPlayer);
+                        Game.DisplaySubtitle("~r~Suspect~w~: Oh, Shit. What's going on here, officer?");
+                    }
+                    if(counter == 3)
+                    {
+                        Game.DisplaySubtitle("~b~You~w~: We have gotten reports of suspicious activity. What's going on here?");
+                    }
+                    if(counter == 4)
+                    {
+                        Game.DisplaySubtitle("~r~Suspect~w~: Fucking your mother, what does it look like I'm doing?");
+                    }
+                    if(counter == 5)
+                    {
+                        Game.DisplaySubtitle("~b~You~w~: What's your name, " + malefemale + "?");
+                    }
+                    if(counter == 6)
+                    {
+                        Game.DisplaySubtitle("~r~Suspect~w~: None of your fucking business, bitch!");
+                    }
+                    if(counter == 7)
+                    {
+                        Game.DisplaySubtitle("~b~You~w~: Identify yourself or you're gonna go to jail. This is your only opportunity to identify yourself.");
+                    }
+                    if(counter == 8)
+                    {
+                        Game.DisplaySubtitle("~r~Suspect~w~: KISS MY MIGHTY BUNGHOLE!");
+                    }
+                    if(counter == 9)
+                    {
+                        Game.DisplaySubtitle("~b~You~w~: You're under arrest, " + malefemale + ", for failing to indentify yourself to a law enforcement officer.");
+                    }
+                    if(counter == 10)
+                    {
+                        Game.DisplaySubtitle("~r~Suspect~w~: I'm not going back to that hell hole. KIFFLOM!!!!!");
+                    }
+                    if(counter == 11)
+                    {
+                        Game.DisplaySubtitle("END OF CONVERSATION!");
+                        suspect.Inventory.GiveNewWeapon(wepList[new Random().Next((int)wepList.Length)], 5000, true);
+                    }
+                }
+            }
+
+            if (Game.IsKeyDown(Settings.EndCall)) End();
+        }
+
+        public override void End()
+        {
+            if (suspect) suspect.Dismiss();
+            if (susBlip) susBlip.Delete();
+            Game.DisplayNotification("web_jonjongames", "web_jonjongames", "~w~JM Callouts Remastered", "~w~Suspicious Person", "~b~You~w~: Dispatch, We are ~g~CODE 4~w~! Show me back 10-8!");
+            LSPD_First_Response.Mod.API.Functions.PlayScannerAudio("ATTENTION_THIS_IS_DISPATCH_HIGH ALL_UNITS_CODE4 NO_FURTHER_UNITS_REQUIRED");
+            base.End();
+
+            Game.LogTrivial("[LOG]: JM Callouts Remastered - Suspicious Person is Code 4!");
         }
     }
 }
