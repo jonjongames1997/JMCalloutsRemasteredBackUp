773b8ddcc4777c9f34c5669e44f40abff8874f56
Jonathan Morrison <87885521+jonjongames1997@users.noreply.github.com>
2024-05-03 15:37:10 -0400

    Update SuspiciousPerson.cs

--- Callouts/SuspiciousPerson.cs
+++ Callouts/SuspiciousPerson.cs
@@ -1,12 +1,37 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using CalloutInterfaceAPI;
 
 namespace JMCalloutsRemastered.Callouts
 {
-    class SuspiciousPerson
+
+    [CalloutInterface("Suspicious Person", CalloutProbability.Medium, "Reports of a suspicious person", "Code 2", "LSPD")]
+
+    public class SuspiciousPerson : Callout
     {
+        private static readonly string[] pedList = new string[] { "ig_abigail", "csb_anita", "ig_shley", "g_m_y_ballaeast_01", "a_m_y_beach_01" };
+        private static readonly string[] wepList = new string[] { "WEAPON_BAT", "WEAPON_SWITCHBLADE", "WEAPON_PISTOL", "WEAPON_PISTOL_MK2", "WEPAON_COMBATPISTOL", "WEAPON_CARBINERIFLE", "WEAPON_TACTICALRIFLE" };
+        private static Ped suspect;
+        private static Vector3 spawnpoint;
+        private static int counter;
+        private static string malefemale;
+        private static Blip susBlip;
+
+        public override bool OnBeforeCalloutDisplayed()
+        {
+            spawnpoint = World.GetNextPositionOnStreet(MainPlayer.Position.Around(1000f));
+            ShowCalloutAreaBlipBeforeAccepting(spawnpoint, 100f);
+            LSPD_First_Response.Mod.API.Functions.PlayScannerAudio("ATTENTION_ALL_UNITS_02 WE_HAVE_01 CRIME_SUSPICIOUS_ACTIVITY UNITS_RESPOND_CODE_02_02");
+            CalloutInterfaceAPI.Functions.SendMessage(this, "Multiple reports of a suspicous person in the area.");
+            CalloutMessage = "Callers report the suspect may be armed.";
+            CalloutPosition = spawnpoint;
+
+            return base.OnBeforeCalloutDisplayed();
+        }
+
+        public override bool OnCalloutAccepted()
+        {
+
+
+            return base.OnCalloutAccepted();
+        }
     }
 }
