4f015b1d9586ac9310aeca9ede5a9985e87cdf37
Jonathan Morrison <87885521+jonjongames1997@users.noreply.github.com>
2024-05-21 14:01:28 -0400

    Create FirstAmendmentAuditors.cs

---
+++ Callouts/FirstAmendmentAuditors.cs
@@ -0,0 +1,19 @@
+﻿using CalloutInterfaceAPI;
+
+namespace JMCalloutsRemastered.Callouts
+{
+
+    [CalloutInterface("1st Amendment Auditor", CalloutProbability.High, "Security reporting an individual refusing to leave government property", "Code 2", "LSPD")]
+
+
+    public class FirstAmendmentAuditors : Callout
+    {
+        private static Ped suspect;
+        private static Blip suspectBlip;
+        private static Vector3 spawnpoint;
+        private static int counter;
+        private static string malefemale;
+
+
+    }
+}
