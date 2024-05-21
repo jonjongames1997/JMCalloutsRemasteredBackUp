247d5a14262cb7a707b66d6153e3d0d9ec1439e9
Jonathan Morrison <87885521+jonjongames1997@users.noreply.github.com>
2024-05-21 14:23:10 -0400

    Update FirstAmendmentAuditors.cs

--- Callouts/FirstAmendmentAuditors.cs
+++ Callouts/FirstAmendmentAuditors.cs
@@ -14,6 +14,23 @@ namespace JMCalloutsRemastered.Callouts
         private static int counter;
         private static string malefemale;
 
+        public override bool OnBeforeCalloutDisplayed()
+        {
+            spawnpoint = World.GetNextPositionOnStreet(MainPlayer.Position.Around(1000f));
+            ShowCalloutAreaBlipBeforeAccepting(spawnpoint, 100f);
+            LSPD_First_Response.Mod.API.Functions.PlayScannerAudio("ATTENTION_ALL_UNITS_01 WE_HAVE_01 CRIME_SECURITY_REQUESTING_ASSISTANCE UNITS_RESPOND_CODE_02_01");
+            CalloutInterfaceAPI.Functions.SendMessage(this, "Security officer requiring assitance.");
+            CalloutMessage = "An individual claiming to be 'The Press' refusing to leave government building.";
+            CalloutPosition = spawnpoint;
 
+            return base.OnBeforeCalloutDisplayed();
+        }
+
+        public override bool OnCalloutAccepted()
+        {
+
+
+            return base.OnCalloutAccepted();
+        }
     }
 }
