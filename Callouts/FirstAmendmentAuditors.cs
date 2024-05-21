9c3c4d2639572412b4fb9f71c7348dcad8e1bcb3
Jonathan Morrison <87885521+jonjongames1997@users.noreply.github.com>
2024-05-21 18:19:44 -0400

    Update FirstAmendmentAuditors.cs

--- Callouts/FirstAmendmentAuditors.cs
+++ Callouts/FirstAmendmentAuditors.cs
@@ -9,7 +9,10 @@ namespace JMCalloutsRemastered.Callouts
     public class FirstAmendmentAuditors : Callout
     {
         private static Ped suspect;
+        private static Ped securityOfficer;
+        private static readonly string[] pedList = new string[] { "mp_s_m_armoured_01", "s_m_m_armoured_01", "s_m_m_armoured_02", "ig_casey", "s_m_m_chemsec_01", "csb_mweather", "s_m_m_security_01", "mp_m_securoguard_01" };
         private static Blip suspectBlip;
+        private static Blip securityBlip;
         private static Vector3 spawnpoint;
         private static int counter;
         private static string malefemale;
@@ -28,9 +31,55 @@ namespace JMCalloutsRemastered.Callouts
 
         public override bool OnCalloutAccepted()
         {
+            Game.LogTrivial("JM Callouts Remastered Log: First Amendment Auditor callout accepted!");
+            Game.DisplayNotification("web_jonjongames", "web_jonjongames", "~w~JM Callouts Remastered", "~w~First Amendment Auditor", "~b~Dispatch~w~: The suspect has been spotted! Respond ~r~Code 2~w~.");
+            Game.DisplayHelp("Press ~y~END~w~ at anytime to end the callout", false);
+
+            suspect = new Ped(spawnpoint);
+            suspect.IsPersistent = true;
+            suspect.BlockPermanentEvents = true;
+
+            suspectBlip = suspect.AttachBlip();
+            suspectBlip.Color = System.Drawing.Color.AliceBlue;
+            suspectBlip.IsRouteEnabled = true;
+
+            securityOfficer = new Ped(pedList[new Random().Next((int)pedList.Length)], spawnpoint, 0f);
+            securityOfficer.IsPersistent = true;
+            securityOfficer.BlockPermanentEvents = true;
+
+            securityBlip = securityOfficer.AttachBlip();
+            securityBlip.Color = System.Drawing.Color.Cornsilk;
+
+            if (suspect.IsMale)
+                malefemale = "Sir";
+            else
+                malefemale = "Ma'am";
+
+            counter = 0;
 
 
             return base.OnCalloutAccepted();
         }
+
+
+        public override void OnCalloutNotAccepted()
+        {
+            if (suspect) suspect.Delete();
+            if (securityOfficer) securityOfficer.Delete();
+            if (suspectBlip) suspectBlip.Delete();
+            if (securityBlip) securityBlip.Delete();
+
+            base.OnCalloutNotAccepted();
+        }
+
+        public override void Process()
+        {
+
+
+
+
+            base.Process();
+        }
+
     }
 }
