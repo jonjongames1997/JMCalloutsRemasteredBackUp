59155349c414becb0d1ebd4d9aceedb1271b718e
Jonathan Morrison <87885521+jonjongames1997@users.noreply.github.com>
2024-05-28 04:29:27 -0400

    Update Rocketman.cs

--- Callouts/Rocketman.cs
+++ Callouts/Rocketman.cs
@@ -51,6 +51,17 @@ namespace JMCalloutsRemastered.Callouts
 
             counter = 0;
 
+            if (Settings.ActivateAIBackup)
+            {
+                LSPD_First_Response.Mod.API.Functions.RequestBackup(spawnpoint, LSPD_First_Response.EBackupResponseType.Code3, LSPD_First_Response.EBackupUnitType.LocalUnit);
+                LSPD_First_Response.Mod.API.Functions.RequestBackup(spawnpoint, LSPD_First_Response.EBackupResponseType.Code3, LSPD_First_Response.EBackupUnitType.SwatTeam);
+                LSPD_First_Response.Mod.API.Functions.RequestBackup(spawnpoint, LSPD_First_Response.EBackupResponseType.Code3, LSPD_First_Response.EBackupUnitType.NooseTeam);
+            }
+            else
+            {
+                Settings.ActivateAIBackup = false;
+            }
+
             return base.OnCalloutAccepted();
         }
 
@@ -96,27 +107,27 @@ namespace JMCalloutsRemastered.Callouts
                     {
                         Game.DisplaySubtitle("~b~You~w~: We've gotten reports of you threatening people with that explosive weapon. Drop it now, I'm not gonna say it again, " + malefemale + ".");
                     }
-                    if(counter == 6)
+                    if (counter == 6)
                     {
                         Game.DisplaySubtitle("~r~Suspect~w~: Go fuck yourself, PO PO!");
                     }
-                    if(counter == 7)
+                    if (counter == 7)
                     {
                         Game.DisplaySubtitle("~b~You~w~: Chief, suspect is not complying with commands and not dropping the weapon.");
                     }
-                    if(counter == 8)
+                    if (counter == 8)
                     {
                         Game.DisplaySubtitle("~b~Chief~w~: If he/she points the weapon at you, ~r~LETHAL FORCE~w~ is authorized.");
                     }
-                    if(counter == 9)
+                    if (counter == 9)
                     {
                         Game.DisplaySubtitle("~b~You~w~: 10-4, Chief.");
                     }
-                    if(counter == 10)
+                    if (counter == 10)
                     {
                         Game.DisplaySubtitle("~b~You~w~: " + malefemale + ", Just drop the weapon, we don't want to shoot you. It's not worth it. Do you have kids?");
                     }
-                    if(counter == 11)
+                    if (counter == 11)
                     {
                         Game.DisplaySubtitle("~r~Suspect~w~: Fuck you! Take your last breath of fresh air, motherfuckers!");
                         suspect.Tasks.FightAgainst(MainPlayer);
