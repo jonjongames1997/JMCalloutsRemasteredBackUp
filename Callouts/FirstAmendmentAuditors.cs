e8b7163d2c5ce463e604657a87a069f2edb04b56
Jonathan Morrison <87885521+jonjongames1997@users.noreply.github.com>
2024-05-28 05:58:07 -0400

    Update FirstAmendmentAuditors.cs

--- Callouts/FirstAmendmentAuditors.cs
+++ Callouts/FirstAmendmentAuditors.cs
@@ -63,6 +63,15 @@ namespace JMCalloutsRemastered.Callouts
 
             counter = 0;
 
+            if (Settings.ActivateAIBackup)
+            {
+                LSPD_First_Response.Mod.API.Functions.RequestBackup(spawnpoint, LSPD_First_Response.EBackupResponseType.Code2, LSPD_First_Response.EBackupUnitType.LocalUnit);
+                LSPD_First_Response.Mod.API.Functions.RequestBackup(spawnpoint, LSPD_First_Response.EBackupResponseType.Code2, LSPD_First_Response.EBackupUnitType.LocalUnit);
+            }
+            else
+            {
+                Settings.ActivateAIBackup = false;
+            }
 
             return base.OnCalloutAccepted();
         }
