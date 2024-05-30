8d33c6d97998d8f190b5edaa62459a4546b15248
Jonathan Morrison <87885521+jonjongames1997@users.noreply.github.com>
2024-05-30 15:19:38 -0400

    Update AssemblyInfo.cs

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -18,6 +18,6 @@ using System.Runtime.Versioning;
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
 [assembly: Guid("a0b80041-3dd3-4086-9fd7-05326b5edf76")]
-[assembly: AssemblyVersion("4.7.2")]
-[assembly: AssemblyFileVersion("4.7.2")]
+[assembly: AssemblyVersion("4.8.2.1")]
+[assembly: AssemblyFileVersion("4.8.2.1")]
 [assembly: NeutralResourcesLanguage("en-US")]
