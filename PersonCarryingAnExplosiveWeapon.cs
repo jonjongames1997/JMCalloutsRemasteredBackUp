45fb55bd1c5077fb49fc6832a03db8fe89bf3dc1
Jonathan Morrison <87885521+jonjongames1997@users.noreply.github.com>
2024-06-06 02:12:52 -0400

    Create PersonCarryingAnExplosiveWeapon.cs

---
+++ PersonCarryingAnExplosiveWeapon.cs
@@ -0,0 +1,27 @@
+﻿using CalloutInterfaceAPI;
+
+namespace JMCalloutsRemastered
+{
+
+
+    [CalloutInterface("[JM Callouts] Person Carrying An Explosive Weapon", CalloutProbability.High, "Citizen's report of an individual carrying an explosive weapon", "CODE 3", "SWAT")]
+
+    public class PersonCarryingAnExplosiveWeapon : Callout
+    {
+
+        private static readonly string[] wepList = new string[] { "WEAPON_RPG", "WEAPON_GRENADELAUNCHER", "WEAPON_GRENADELAUNCHER_SMOKE", "WEAPON_FIREWORK", "WEAPON_HOMINGLAUNCHER", "WEAPON_COMPACTLAUNCHER", "WEAPON_GRENADE", "WEAPON_MOLOTOV", "WEAPON_STICKYBOMB", "WEAPON_PROXMINE", "WEAPON_PIPEBOMB" };
+        private static Ped suspect;
+        private static Vector3 spawnpoint;
+        private static Blip suspectBlip;
+        private static int counter;
+        private static string malefemale;
+
+
+        public override bool OnBeforeCalloutDisplayed()
+        {
+
+
+            return base.OnBeforeCalloutDisplayed();
+        }
+    }
+}
