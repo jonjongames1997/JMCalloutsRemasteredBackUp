aa115f4e9fd6a153f71e5227ada49f327ac86e3a
Jonathan Morrison <87885521+jonjongames1997@users.noreply.github.com>
2024-06-11 04:34:30 -0400

    Update RefuseToLeave.cs

--- Callouts/RefuseToLeave.cs
+++ Callouts/RefuseToLeave.cs
@@ -47,6 +47,11 @@ namespace JMCalloutsRemastered.Callouts
             Game.DisplayNotification("web_jonjongames", "web_jonjongames", "~w~JM Callouts Remastered", "~w~Refuse To Leave", "~b~Dispatch~w~: Suspect has been spotted. Respond ~r~Code 1~w~.");
             Game.DisplayHelp("Press ~y~END~w~ at anytime to end the callout", false);
 
+            Settings.CallsAccepted++;
+            Settings.Stats.SelectSingleNode("Stats/CallsAccepted").InnerText = Settings.CallsAccepted.ToString();
+            Settings.Stats.SelectSingleNode("Stats/Pursuits").InnerText = Settings.Pursuits.ToString();
+            Settings.Stats.Save(Settings.xmlpath);
+
             Suspect = new Ped(Spawnpoint);
             Suspect.IsPersistent = true;
             Suspect.BlockPermanentEvents = true;
