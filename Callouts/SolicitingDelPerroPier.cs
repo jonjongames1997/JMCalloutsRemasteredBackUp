264a74c77f9182a3fba26576b04d05e932d758d1
Jonathan Morrison <87885521+jonjongames1997@users.noreply.github.com>
2024-06-11 15:34:35 -0400

    Update SolicitingDelPerroPier.cs

--- Callouts/SolicitingDelPerroPier.cs
+++ Callouts/SolicitingDelPerroPier.cs
@@ -33,10 +33,6 @@ namespace JMCalloutsRemastered.Callouts
             Game.DisplayNotification("web_jonjongames", "web_jonjongames", "~w~JM Callouts Remastered", "~w~Soliciting - Del Perro", "~b~Dispatch~w~: The suspect has been spotted! Respond ~r~Code 2~w~.");
             Game.DisplayHelp("Press ~y~END~w~ at anytime to end the callout", false);
 
-            Settings.CallsAccepted++;
-            Settings.Stats.SelectSingleNode("Stats/CallsAccepted").InnerText = Settings.CallsAccepted.ToString();
-            Settings.Stats.SelectSingleNode("Stats/Shootouts").InnerText = Settings.Shootouts.ToString();
-            Settings.Stats.Save(Settings.xmlpath);
 
             suspect = new Ped(spawnpoint, heading);
             suspect.IsPersistent = true;
