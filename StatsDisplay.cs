3f0f638569e773f4d152314d7f04e529d0ad02b9
Jonathan Morrison <87885521+jonjongames1997@users.noreply.github.com>
2024-06-11 01:58:42 -0400

    Create StatsDisplay.cs

---
+++ StatsDisplay.cs
@@ -0,0 +1,16 @@
+﻿namespace JMCalloutsRemastered
+{
+    internal class StatsDisplay
+    {
+        public static void DisplayStats()
+        {
+            Game.DisplayNotification("web_jonjongames", "web_jonjongames", "~w~JM Callouts Remastered", "~w~Patrol Stats", string.Format("~s~Arrest: ~o~{0} ~s~~n~Fights Involed In: ~o~{1} ~n~~s~Pursuts: ~o~{2} ~n~~s~Calls Accepted: ~o~{3}", new object[]
+            {
+                Settings.Arrests.ToString(),
+                Settings.FightsInvolved.ToString(),
+                Settings.Pursuits.ToString(),
+                Settings.CallsAccepted
+            }));
+        }
+    }
+}
