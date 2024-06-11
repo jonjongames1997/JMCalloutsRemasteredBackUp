d19e2bb324e6066603b735beefe54b84c1fb0dd4
Jonathan Morrison <87885521+jonjongames1997@users.noreply.github.com>
2024-06-11 04:35:23 -0400

    Update StatsDisplay.cs

--- StatsDisplay.cs
+++ StatsDisplay.cs
@@ -4,12 +4,13 @@
     {
         public static void DisplayStats()
         {
-            Game.DisplayNotification("web_jonjongames", "web_jonjongames", "~w~JM Callouts Remastered", "~w~Patrol Stats", string.Format("~s~Arrest: ~o~{0} ~s~~n~Fights Involed In: ~o~{1} ~n~~s~Pursuts: ~o~{2} ~n~~s~Calls Accepted: ~o~{3}", new object[]
+            Game.DisplayNotification("web_jonjongames", "web_jonjongames", "~w~JM Callouts Remastered", "~w~Patrol Stats", string.Format("~s~Arrest: ~o~{0} ~s~~n~Fights Involed In: ~o~{1} ~n~~s~Pursuts: ~o~{2} ~n~~s~Calls Accepted: ~o~{3} ~n~~s~Shootouts: ~o~{4}", new object[]
             {
                 Settings.Arrests.ToString(),
                 Settings.FightsInvolved.ToString(),
                 Settings.Pursuits.ToString(),
-                Settings.CallsAccepted
+                Settings.CallsAccepted.ToString(),
+                Settings.Shootouts.ToString(),
             }));
         }
     }
