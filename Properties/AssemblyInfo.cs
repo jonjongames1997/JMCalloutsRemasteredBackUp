244b3a88ab9bcdf07d93b7f44b2fe98dfe4e7ab1
Jonathan Morrison <87885521+jonjongames1997@users.noreply.github.com>
2024-06-19 01:27:27 -0400

    Update AssemblyInfo.cs

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -1,13 +1,9 @@
-﻿using System.Diagnostics;
-using System.Reflection;
+﻿using System.Reflection;
 using System.Resources;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 using System.Runtime.Versioning;
 
-[assembly: CompilationRelaxations(8)]
-[assembly: RuntimeCompatibility(WrapNonExceptionThrows = true)]
-[assembly: Debuggable(DebuggableAttribute.DebuggingModes.Default | DebuggableAttribute.DebuggingModes.DisableOptimizations | DebuggableAttribute.DebuggingModes.IgnoreSymbolStoreSequencePoints | DebuggableAttribute.DebuggingModes.EnableEditAndContinue)]
 [assembly: AssemblyTitle("JMCalloutsRemastered")]
 [assembly: AssemblyDescription("LSPDFR Callout Pack for LSPDFR Police Mod")]
 [assembly: AssemblyConfiguration("")]
