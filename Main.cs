97d350ca681ae4585c9043bfdad78b15c24002f4
Jonathan Morrison <87885521+jonjongames1997@users.noreply.github.com>
2024-06-25 08:14:53 -0400

    Update Main.cs

--- Main.cs
+++ Main.cs
@@ -133,7 +133,6 @@ namespace JMCalloutsRemastered
             if (Settings.Rocketman) { Functions.RegisterCallout(typeof(Rocketman)); }
             if (Settings.PrankCall) { Functions.RegisterCallout(typeof(PrankCall)); }
             if (Settings.InjuredPerson) { Functions.RegisterCallout(typeof(InjuredPerson)); }
-            if (Settings.UnknownTrouble) { Functions.RegisterCallout(typeof(UnknownTrouble)); }
             if (Settings.PersonCarryingAnExplosiveWeapon) { Functions.RegisterCallout(typeof(PersonCarryingAnExplosiveWeapon)); }
             Game.Console.Print("[LOG]: All callouts of the JMCalloutsRemastered.ini were loaded successfully.");
             Game.Console.Print();
