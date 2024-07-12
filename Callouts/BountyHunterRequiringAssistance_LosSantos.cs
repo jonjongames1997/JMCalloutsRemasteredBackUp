ea96e76c3f24d457484960058349fc0cdbf56911
Jonathan Morrison <87885521+jonjongames1997@users.noreply.github.com>
2024-07-12 17:30:18 -0400

    Create BountyHunterRequiringAssistance_LosSantos.cs

---
+++ Callouts/BountyHunterRequiringAssistance_LosSantos.cs
@@ -0,0 +1,113 @@
+﻿using CalloutInterfaceAPI;
+
+namespace JMCalloutsRemastered.Callouts
+{
+
+
+    [CalloutInterface("Bounty Hunter Requiring Assistance - Los Santos", CalloutProbability.Medium, "A bounty hunter requiring assistance", "CODE 2", "SWAT")]
+
+    public class BountyHunterRequiringAssistance_LosSantos : Callout
+    {
+
+        // General Variables //
+        private static Ped suspect;
+        private static Vector3 spawnpoint;
+        private static Blip susBlip;
+        private static int counter;
+        private static string malefemale;
+
+
+        public override bool OnBeforeCalloutDisplayed()
+        {
+            spawnpoint = World.GetNextPositionOnStreet(MainPlayer.Position.Around(1000f));
+            ShowCalloutAreaBlipBeforeAccepting(spawnpoint, 100f);
+            CalloutInterfaceAPI.Functions.SendMessage(this, "A bounty hunter requiring asssitance");
+            CalloutMessage = "A bounty hunter needing assistance with bringing a suspect into custody";
+            CalloutPosition = spawnpoint;
+
+            return base.OnBeforeCalloutDisplayed();
+        }
+
+
+        public override bool OnCalloutAccepted()
+        {
+            Game.LogTrivial("[JM Callouts Remastered Log]: Bounty Hunter Requiring Assistance - Los Santos callout accepted!");
+            Game.DisplayNotification("web_jonjongames", "web_jonjongames", "~w~JM Callouts Remastered", "~w~Bounty Hunter Requiring Assistance - Los Santos", "~b~Dispatch:~w~ Suspect has been spotted. Respond ~r~Code 3.");
+            Game.DisplayHelp("Press ~y~END~w~ at anytime to end the callout", false);
+
+            suspect = new Ped(spawnpoint);
+            suspect.IsPersistent = true;
+            suspect.BlockPermanentEvents = true;
+            suspect.IsValid();
+
+            susBlip = suspect.AttachBlip();
+            susBlip.Color = System.Drawing.Color.Red;
+            susBlip.IsRouteEnabled = true;
+
+            if (suspect.IsMale)
+                malefemale = "Sir";
+            else
+                malefemale = "Ma'am";
+
+            counter = 0;
+
+            return base.OnCalloutAccepted();
+        }
+
+        public override void OnCalloutNotAccepted()
+        {
+            if (suspect) suspect.Delete();
+            if (susBlip) susBlip.Delete();
+
+            base.OnCalloutNotAccepted();
+        }
+
+        public override void Process()
+        {
+            base.Process();
+
+            if(MainPlayer.DistanceTo(suspect) <= 10f)
+            {
+
+                Game.DisplayHelp("Press ~y~E~w~ to interact with ~r~suspect~w~.", false);
+
+                if (Game.IsKeyDown(System.Windows.Forms.Keys.E))
+                {
+                    counter++;
+
+                    if(counter == 1)
+                    {
+                        Game.DisplaySubtitle("~b~You~w~: SWAT Police! Stop right where you are and surrender carefully! Reach for anything, you'll be shot graveyard dead.");
+                    }
+                    if(counter == 2)
+                    {
+                        Game.DisplaySubtitle("~r~Suspect~w~: You'll never take me alive coppers!");
+                    }
+                    if(counter == 3)
+                    {
+                        Game.DisplaySubtitle("Conversation Ended!");
+                        suspect.Inventory.GiveNewWeapon("WEAPON_COMBATMG", 500, true);
+                        suspect.Tasks.FightAgainst(MainPlayer);
+                        suspect.Armor = 500;
+                    }
+                }
+            }
+
+            if (MainPlayer.IsDead) End();
+            if (Game.IsKeyDown(Settings.EndCall)) End();
+        }
+
+        public override void End()
+        {
+            if (suspect) suspect.Dismiss();
+            if (susBlip) susBlip.Delete();
+            Game.DisplayNotification("web_jonjongames", "web_jonjongames", "~w~JM Callouts Remastered", "~w~Bounty Hunter Requiring Assistance - Los Santos", "~b~You~w~: Dispatch, we are ~g~Code 4~w~. Show me back 10-8.");
+            LSPD_First_Response.Mod.API.Functions.PlayScannerAudio("ATTENTION_THIS_IS_DISPATCH_HIGH ALL_UNITS_CODE4 NO_FURTHER_UNITS_REQUIRED");
+
+            base.End();
+
+            Game.LogTrivial("JM Callouts Remastered - Bounty Hunter Requiring Assistance - Los Santos is Code 4!");
+        }
+
+    }
+}
