2d3a370c4af1f275619d63814c4309b780012b51
Jonathan Morrison <87885521+jonjongames1997@users.noreply.github.com>
2024-07-21 21:28:46 -0400

    Update Main.cs

--- Main.cs
+++ Main.cs
@@ -136,7 +136,7 @@ namespace JMCalloutsRemastered
             if (Settings.PrankCall) { Functions.RegisterCallout(typeof(PrankCall)); }
             if (Settings.InjuredPerson) { Functions.RegisterCallout(typeof(InjuredPerson)); }
             if (Settings.PersonCarryingAnExplosiveWeapon) { Functions.RegisterCallout(typeof(PersonCarryingAnExplosiveWeapon)); }
-            if (Settings.BountyHunterRequiringAssistanceLosSantos) { Functions.RegisterCallout(typeof(BountyHunterRequiringAssistance_LosSantos)); }
+            if (Settings.BountyHunterRequiringAssistance) { Functions.RegisterCallout(typeof(BountyHunterRequiringAssistance)); }
             Game.Console.Print("[LOG]: All callouts of the JMCalloutsRemastered.ini were loaded successfully.");
             Game.Console.Print();
             Game.Console.Print("================================================== JM Callouts Remastered ===================================================");
