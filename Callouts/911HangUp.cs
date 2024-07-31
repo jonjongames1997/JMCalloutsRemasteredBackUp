c71c785f463c2bf1ff99e21f942285e33c0bf83e
Jonathan Morrison <87885521+jonjongames1997@users.noreply.github.com>
2024-07-31 00:34:41 -0400

    Update 911HangUp.cs

--- Callouts/911HangUp.cs
+++ Callouts/911HangUp.cs
@@ -26,7 +26,7 @@ namespace JMCalloutsRemastered.Callouts
                 new(-1335.80f, -929.51f, 11.75f), // Motel Near Rob's Liquors
                 new(-232.05f, -2055.64f, 27.62f), // Maze Bank Arena Parking 
                 new(139.11f, -1635.78f, 29.30f), // Near Ron Station on Davis Ave
-                new(130.16F, -1737.21f, 30.11f), // Train Station next to Davis Mall
+                new(130.16f, -1737.21f, 30.11f), // Train Station next to Davis Mall
                 new(-1324.74f, -397.55f, 35.83f), // Ammunation in Morningwood 
                 new(332.11f, 180.63f, 103.11f), // Oriental Theatre on Vinewood Blvd.
                 new(344.79f, -191.55f, 57.23f), // OCRP Postal 582
@@ -68,7 +68,7 @@ namespace JMCalloutsRemastered.Callouts
 
             SuspectBlip = Suspect.AttachBlip();
             Suspect.Tasks.PutHandsUp(500, MainPlayer);
-            SuspectBlip.Color = System.Drawing.Color.BlueViolet;
+            SuspectBlip.Color = System.Drawing.Color.Yellow;
             SuspectBlip.IsRouteEnabled = true;
 
             if (Suspect.IsMale)
@@ -93,7 +93,7 @@ namespace JMCalloutsRemastered.Callouts
         {
             base.Process();
 
-            if (MainPlayer.DistanceTo(Suspect) <= 10f)
+            if (MainPlayer.DistanceTo(Suspect) <= 5f)
             {
 
                 Game.DisplayHelp("Press ~y~E~w~ to speak with the ~r~suspect~w~.", false);
