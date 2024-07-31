427fb9b6f320e1d24ec95729ee9b60c4d28666f3
Jonathan Morrison <87885521+jonjongames1997@users.noreply.github.com>
2024-07-31 00:35:17 -0400

    Create Arrays.cs

---
+++ Stuff/Arrays.cs
@@ -0,0 +1,18 @@
+﻿namespace JMCalloutsRemastered.Stuff
+{
+    internal class Arrays
+    {
+
+        internal static readonly string[] pluginUnloadText =
+        {
+            "Error Code: KaR3nMoD3",
+            "Is that you Venoxity? 0_0",
+            "You fucked up, didn't you?",
+            "You getting that promotion, Officer.",
+            "I'm shocked that you made it alive?",
+            "Go treat yourself with a beer or something.",
+        };
+
+
+    }
+}
