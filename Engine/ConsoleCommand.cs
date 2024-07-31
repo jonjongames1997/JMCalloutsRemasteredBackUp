457a8d6ba2af2a57edde5a58be1a006250754a7e
Jonathan Morrison <87885521+jonjongames1997@users.noreply.github.com>
2024-07-31 00:34:56 -0400

    Create ConsoleCommand.cs

---
+++ Engine/ConsoleCommand.cs
@@ -0,0 +1,13 @@
+﻿using Rage.Attributes;
+
+namespace JMCalloutsRemastered.Engine
+{
+    public static class ConsoleCommand
+    {
+        [ConsoleCommand]
+        public static void Command_JMCalloutsReloadConfig()
+        {
+            Settings.LoadSettings();
+        }
+    }
+}
