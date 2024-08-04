3f9ce34a8e51741cea545d9e8430db58914bddc0
Jonathan Morrison <87885521+jonjongames1997@users.noreply.github.com>
2024-08-03 22:17:57 -0400

    Update Settings.cs

--- Settings.cs
+++ Settings.cs
@@ -44,6 +44,7 @@ namespace JMCalloutsRemastered
         internal static bool BountyHunterRequiringAssistance = false;
         internal static bool HomelessPerson = true;
         internal static bool HelpMessages = true;
+        internal static bool LeaveCalloutsRunning = true;
         internal static Keys EndCall = Keys.End;
         internal static Keys Dialog = Keys.E;
         internal static InitializationFile ini;
@@ -92,6 +93,7 @@ namespace JMCalloutsRemastered
             Settings.BountyHunterRequiringAssistance = ini.ReadBoolean("Callouts", "BountyHunterRequiringAssistance", false);
             Settings.HomelessPerson = ini.ReadBoolean("Callouts", "HomelessPerson", true);
             Settings.HelpMessages = ini.ReadBoolean("HelpMessages", "Help Messages", true);
+            Settings.LeaveCalloutsRunning = ini.ReadBoolean("Miscellaneous", "LeaveCalloutsRunning", true);
             EndCall = ini.ReadEnum("Keys", "EndCall", Keys.End);
             Dialog = ini.ReadEnum("Keys", "Dialog", Keys.E);
         }
