a99f23b4d92bd2ecd664d7d0098ef4680bfb2270
Jonathan Morrison <87885521+jonjongames1997@users.noreply.github.com>
2024-08-03 22:17:31 -0400

    Create Dependency.cs

---
+++ API/Dependency.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JMCalloutsRemastered.API
+{
+    internal class Dependency
+    {
+        internal string PluginName { get; set; }
+        internal string DependName { get; set; }
+        internal string DependVersion { get; set; }
+    }
+}
