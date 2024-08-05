ad22eed5cf17c8afa4ce1ea8a23042e7afc11292
Jonathan Morrison <87885521+jonjongames1997@users.noreply.github.com>
2024-08-05 15:26:10 -0400

    Update ConsoleCommand.cs

--- Engine/ConsoleCommand.cs
+++ Engine/ConsoleCommand.cs
@@ -2,9 +2,10 @@
 
 namespace JMCalloutsRemastered.Engine
 {
+    
     public static class ConsoleCommand
     {
-        [ConsoleCommand]
+        [Rage.Attributes.ConsoleCommand]
         public static void Command_JMCalloutsReloadConfig()
         {
             Settings.LoadSettings();
