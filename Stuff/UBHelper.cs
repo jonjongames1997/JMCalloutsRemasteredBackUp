1630eba9b29c6afe26ecebe2a2bdff9fb7b73423
Jonathan Morrison <87885521+jonjongames1997@users.noreply.github.com>
2024-08-20 06:29:11 -0400

    Create UBHelper.cs

---
+++ Stuff/UBHelper.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JMCalloutsRemastered.Stuff
+{
+    internal class UltimateBackupFunctions
+    {
+        internal static void CallPursuitBackup()
+        {
+            try
+            {
+                UltimateBackup.API.Functions.callPursuitBackup();
+            }
+            catch (FileNotFoundException)
+            {
+
+            }
+            catch (Exception)
+            {
+
+            }
+        }
+
+        internal static void callCode3BackUp()
+        {
+            try
+            {
+                UltimateBackup.API.Functions.callCode3Backup();
+            }
+            catch (FileNotFoundException)
+            {
+
+            }
+            catch (Exception)
+            {
+
+            }
+        }
+
+        internal static void callCode3SwatBackup(bool radio = false, bool isNooseUnit = false)
+        {
+            try
+            {
+                UltimateBackup.API.Functions.callCode3SwatBackup(radio, isNooseUnit);
+            }
+            catch (FileNotFoundException)
+            {
+
+            }
+            catch (Exception)
+            {
+
+            }
+        }
+
+        internal static void callFireDepartment()
+        {
+            try
+            {
+                UltimateBackup.API.Functions.callFireDepartment();
+            }
+            catch (FileNotFoundException)
+            {
+
+            }
+            catch (Exception)
+            {
+
+            }
+        }
+
+        internal static void callTrafficStopBackup(bool radio = false, bool isLocalPatrol = false)
+        {
+            try
+            {
+                UltimateBackup.API.Functions.callTrafficStopBackup(radio, isLocalPatrol);
+            }
+            catch (FileNotFoundException)
+            {
+
+            }
+            catch (Exception)
+            {
+
+            }
+        }
+    }
+}
