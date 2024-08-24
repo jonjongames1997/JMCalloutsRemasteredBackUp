dffd8829f7c27af4e28c265ee9b6c33533821e9c
Jonathan Morrison <87885521+jonjongames1997@users.noreply.github.com>
2024-08-24 17:28:03 -0400

    Update Arrays.cs
    credit to AstroBurgers for the code reference

--- Stuff/Arrays.cs
+++ Stuff/Arrays.cs
@@ -1,9 +1,20 @@
-﻿namespace JMCalloutsRemastered.Stuff
+﻿using JMCalloutsRemastered.Stuff;
+
+namespace JMCalloutsRemastered.Stuff
 {
     internal class Arrays
     {
 
-        internal static readonly string[] pluginUnloadText =
+        internal static readonly string[] PluginLoadText =
+        {
+            "Patrol Objective: Don't get shot at.",
+            "Patrol Objective: Protect the citizens.",
+            "Patrol Objective: Protect Kifflom!",
+            "Patrol Objective: Don't be a douchebag to the homeless.",
+            "Patrol Tip: Always Watch Your Six.",
+        };
+
+        internal static readonly string[] PluginUnloadText =
         {
             "Error Code: KaR3nMoD3",
             "Is that you Venoxity? 0_0",
