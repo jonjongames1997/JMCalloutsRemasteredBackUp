c32c25b7c969f496a5fe5dd04df9261f206f87e3
Jonathan Morrison <87885521+jonjongames1997@users.noreply.github.com>
2024-08-24 17:27:40 -0400

    Create EnumExtensionManager.cs
    credit to AstroBurgers for the code reference

---
+++ Engine/EnumExtensionManager.cs
@@ -0,0 +1,11 @@
+﻿namespace JMCalloutsRemastered.Engine
+{
+    internal static class EnumExtensionManager
+    {
+        internal static T PickRandom<T>(this IEnumerable<T> source) => source.Any() ? source.PickRandom(1).Single() : default;
+
+        internal static IEnumerable<T> PickRandom<T>(this IEnumerable<T> source, int count) => source.Shuffle().Take(count);
+
+        internal static IEnumerable<T> Shuffle<T>(this IEnumerable<T> source) => source.OrderBy(_ => Guid.NewGuid());
+    }
+}
