6323c646c96fdcb5529e6b446165dba6841f2f96
Jonathan Morrison <87885521+jonjongames1997@users.noreply.github.com>
2024-08-28 04:26:02 -0400

    Create DomesticDisturbanceSandyShores.cs

---
+++ Callouts/DomesticDisturbanceSandyShores.cs
@@ -0,0 +1,14 @@
+﻿using CalloutInterfaceAPI;
+
+namespace JMCalloutsRemastered.Callouts
+{
+
+
+    [CalloutInterface("[JM Callouts] Domestic Disturbance - Sandy Shores", CalloutProbability.Medium, "A couple having a argument", "Code 2", "BCSO")]
+
+
+    public class DomesticDisturbanceSandyShores : Callout
+    { 
+
+    }
+}
