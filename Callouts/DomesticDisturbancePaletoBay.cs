05eef074dd48b55e1705f9553f59fbadfe79b2bb
Jonathan Morrison <87885521+jonjongames1997@users.noreply.github.com>
2024-08-28 04:26:00 -0400

    Create DomesticDisturbancePaletoBay.cs

---
+++ Callouts/DomesticDisturbancePaletoBay.cs
@@ -0,0 +1,12 @@
+﻿using CalloutInterfaceAPI;
+
+namespace JMCalloutsRemastered.Callouts
+{
+
+    [CalloutInterface("[JM Callouts] Domestic Disturbance", CalloutProbability.Medium, "A couple of people arguing", "Code 2", "PBPD")]
+
+    public class DomesticDisturbancePaletoBay : Callout
+    {
+
+    }
+}
