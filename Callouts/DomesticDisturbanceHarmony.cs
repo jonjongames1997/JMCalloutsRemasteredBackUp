c87e3a0fbcf1c54d42c35bf1226cf733b80a92fb
Jonathan Morrison <87885521+jonjongames1997@users.noreply.github.com>
2024-08-28 04:25:57 -0400

    Create DomesticDisturbanceHarmony.cs

---
+++ Callouts/DomesticDisturbanceHarmony.cs
@@ -0,0 +1,15 @@
+﻿using CalloutInterfaceAPI;
+
+namespace JMCalloutsRemastered.Callouts
+{
+
+
+    [CalloutInterface("[JM Callouts] Domestic Disturbance - Harmony", CalloutProbability.Medium, "A couple of people arguing", "Code 2", "BCSO")]
+
+    public class DomesticDisturbanceHarmony : Callout
+    {
+
+
+
+    }
+}
