93336b34862577df12b160a999d1ffd7f1dd89f3
Jonathan Morrison <87885521+jonjongames1997@users.noreply.github.com>
2024-09-02 16:55:42 -0400

    Update JMCalloutsRemasteredAPI.cs

--- API/JMCalloutsRemasteredAPI.cs
+++ API/JMCalloutsRemasteredAPI.cs
@@ -10,48 +10,25 @@ namespace JMCalloutsRemastered.Engine
 {
     internal static class JMCalloutsRemasteredAPI
     {
-        private static List<Dependency> _depends = new List<Dependency>();
-
-        internal static void AddDepend(string name, string version)
+        // Credit to Opus49 for this method
+        internal static bool IsAssemblyAvailable(string assemblyName, string version)
         {
-            var newDepend = new Dependency()
+            try
             {
-                PluginName = Assembly.GetExecutingAssembly().FullName.Split('.').First(),
-                DependName = name,
-                DependVersion = version
-            };
-            if (!_depends.Contains(newDepend)) _depends.Add(newDepend);
-        }
-
-        internal static bool CheckDepends()
-        {
-            var PluginName = Assembly.GetExecutingAssembly().FullName.Split('.').First();
-            var missingDepend = string.Empty;
-            var OutdatedDependency = string.Empty;
-            var pluginDepends = _depends.Where(depend => depend.PluginName == PluginName).ToList();
-
-            foreach (var depend in pluginDepends)
-                if (!File.Exists(depend.PluginName)) missingDepend += $"{depend.DependName}~n~";
-            if(missingDepend.Length > 0)
-            {
-                Logger.Debug($"You are missing a dependency that JM Callouts Remastered relies on!\r\n{missingDepend.Replace("~n~", "\r\n")}{PluginName} could not load!");
-                Game.DisplayNotification("new_editor", "warningtriangle", $"~r~{PluginName}", "~r~Not Loaded!", "Plugin is installed incorrectly! Please see the RagePluginHook.log! Get support here: https://discord.gg/N9KgZx4KUn");
+                var assemblyName2 = AssemblyName.GetAssemblyName(AppDomain.CurrentDomain.BaseDirectory + "/" + assemblyName);
+                if (assemblyName2.Version >= new Version(version))
+                {
+                    Normal($"{assemblyName} is available ({assemblyName2.Version}).");
+                    return true;
+                }
+                Normal($"{assemblyName} does not meet minimum requirements ({assemblyName2.Version} < {version}).");
                 return false;
             }
-
-            foreach(var depend in pluginDepends)
+            catch (Exception ex) when (ex is FileNotFoundException or BadImageFormatException)
             {
-                var dependVersion = new Version(FileVersionInfo.GetVersionInfo(depend.DependName).FileVersion);
-                if (dependVersion < new Version(depend.DependVersion)) OutdatedDependency += $"{depend.DependName}~n~";
-            }
-
-            if(OutdatedDependency.Length > 0)
-            {
-                Logger.Debug($"There are dependencies which are outdated! Please update as soon as possible.\r\n{OutdatedDependency.Replace("~n~", "\r\n")}{PluginName} could not load!");
-                Game.DisplayNotification("new_editor", "warningtriangle", $"~r~{PluginName}", "~r~Not Loaded!", "Plugin is installed incorrectly!Please see the RagePluginHook.log! Get support here: https://discord.gg/N9KgZx4KUn");
+                Normal(assemblyName + " is not available.");
                 return false;
             }
-            return true;
         }
     }
 }
