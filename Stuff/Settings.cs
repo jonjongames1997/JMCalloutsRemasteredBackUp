5b02d89b2e1848304729a6705a77b8e83148799b
jonjongames1997 <87885521+jonjongames1997@users.noreply.github.com>
2024-10-06 01:42:31 -0400

    Update Settings.cs

--- Stuff/Settings.cs
+++ Stuff/Settings.cs
@@ -105,6 +105,6 @@ namespace JMCalloutsRemastered
             EndCall = ini.ReadEnum("Keys", "EndCall", Keys.End);
             Dialog = ini.ReadEnum("Keys", "Dialog", Keys.E);
         }
-        public static readonly string PluginVersion = "5.0.5.3";
+        public static readonly string PluginVersion = "5.0.6";
     }
 }
