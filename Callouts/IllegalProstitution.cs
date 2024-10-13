e51e96ced35f768db96254d221d3e6874b01a17e
jonjongames1997 <87885521+jonjongames1997@users.noreply.github.com>
2024-10-12 21:15:17 -0400

    Update IllegalProstitution.cs

--- Callouts/IllegalProstitution.cs
+++ Callouts/IllegalProstitution.cs
@@ -135,8 +135,15 @@ namespace JMCalloutsRemastered.Callouts
                 }
             }
 
-            if (MainPlayer.IsDead) End();
-            if (Game.IsKeyDown(Settings.EndCall)) End();
+            if (MainPlayer.IsDead)
+            {
+                End();
+            }
+
+            if (Game.IsKeyDown(Settings.EndCall))
+            {
+                End();
+            }
         }
 
         public override void End()
