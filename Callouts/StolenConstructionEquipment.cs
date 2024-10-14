6747ae0f69d0f251fc20c37aebaef841c353ef25
jonjongames1997 <87885521+jonjongames1997@users.noreply.github.com>
2024-10-14 03:52:39 -0400

    Update StolenConstructionEquipment.cs

--- Callouts/StolenConstructionEquipment.cs
+++ Callouts/StolenConstructionEquipment.cs
@@ -69,8 +69,15 @@ namespace JMCalloutsRemastered.Callouts
 
         public override void Process()
         {
-            if (MainPlayer.IsDead) End();
-            if (Game.IsKeyDown(Settings.EndCall)) End();
+            if (MainPlayer.IsDead)
+            {
+                End();
+            }
+
+            if (Game.IsKeyDown(Settings.EndCall))
+            {
+                End();
+            }
 
             base.Process();
         }
