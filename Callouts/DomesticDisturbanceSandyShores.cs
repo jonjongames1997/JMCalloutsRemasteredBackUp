d42fc23c64c3c1199a7b7f3ab1292803bf7a0e4f
jonjongames1997 <87885521+jonjongames1997@users.noreply.github.com>
2024-10-14 03:51:56 -0400

    Update DomesticDisturbanceSandyShores.cs

--- Callouts/DomesticDisturbanceSandyShores.cs
+++ Callouts/DomesticDisturbanceSandyShores.cs
@@ -121,8 +121,15 @@ namespace JMCalloutsRemastered.Callouts
                 }
             }
 
-            if (MainPlayer.IsDead) End();
-            if (Game.IsKeyDown(Settings.EndCall)) End();
+            if (MainPlayer.IsDead)
+            {
+                End();
+            }
+
+            if (Game.IsKeyDown(Settings.EndCall))
+            {
+                End();
+            }
 
         }
 
