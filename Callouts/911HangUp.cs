abccdc1c3777dee6d31821ca10f486dfca40bc54
jonjongames1997 <87885521+jonjongames1997@users.noreply.github.com>
2024-10-14 03:51:35 -0400

    Update 911HangUp.cs

--- Callouts/911HangUp.cs
+++ Callouts/911HangUp.cs
@@ -145,8 +145,15 @@ namespace JMCalloutsRemastered.Callouts
                 }
             }
 
-            if (MainPlayer.IsDead) End();
-            if (Game.IsKeyDown(Settings.EndCall)) End();
+            if (MainPlayer.IsDead)
+            {
+                End();
+            }
+
+            if (Game.IsKeyDown(Settings.EndCall))
+            {
+                End();
+            }
         }
 
         public override void End()
