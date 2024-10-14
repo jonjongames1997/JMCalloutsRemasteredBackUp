58a1fdddf3b06da0de3ea5332c9dfd38f250daea
jonjongames1997 <87885521+jonjongames1997@users.noreply.github.com>
2024-10-14 03:51:49 -0400

    Update DomesticDisturbanceGrapeSeed.cs

--- Callouts/DomesticDisturbanceGrapeSeed.cs
+++ Callouts/DomesticDisturbanceGrapeSeed.cs
@@ -128,7 +128,15 @@ namespace JMCalloutsRemastered.Callouts
                 }
             }
 
-            if (Game.IsKeyDown(Settings.EndCall)) End();
+            if (MainPlayer.IsDead)
+            {
+                End();
+            }
+
+            if (Game.IsKeyDown(Settings.EndCall))
+            {
+                End();
+            }
 
         }
 
