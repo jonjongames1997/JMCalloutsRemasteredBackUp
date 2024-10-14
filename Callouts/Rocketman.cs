8ab00674b7fcd64c3b2e675871bfb1d4f92ed2b9
jonjongames1997 <87885521+jonjongames1997@users.noreply.github.com>
2024-10-14 03:52:34 -0400

    Update Rocketman.cs

--- Callouts/Rocketman.cs
+++ Callouts/Rocketman.cs
@@ -130,8 +130,15 @@ namespace JMCalloutsRemastered.Callouts
                 }
             }
 
-            if (MainPlayer.IsDead) End();
-            if (Game.IsKeyDown(Settings.EndCall)) End();
+            if (MainPlayer.IsDead)
+            {
+                End();
+            }
+
+            if (Game.IsKeyDown(Settings.EndCall))
+            {
+                End();
+            }
 
         }
 
