d7a23a08de900fe6a78c6f3cb12aa67c4a0622e7
jonjongames1997 <87885521+jonjongames1997@users.noreply.github.com>
2024-10-14 03:52:18 -0400

    Update PersonOnTheHighway.cs

--- Callouts/PersonOnTheHighway.cs
+++ Callouts/PersonOnTheHighway.cs
@@ -150,8 +150,15 @@ namespace JMCalloutsRemastered.Callouts
                     }
                 }
 
-                if (MainPlayer.IsDead) End();
-                if (Game.IsKeyDown(Settings.EndCall)) End();
+                if (MainPlayer.IsDead)
+                {
+                    End();
+                }
+
+                if (Game.IsKeyDown(Settings.EndCall))
+                {
+                    End();
+                }
             }
         }
 
