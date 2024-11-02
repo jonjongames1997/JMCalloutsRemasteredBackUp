5398b6f2c8c7c8672312dad8c67577f353435794
jonjongames1997 <87885521+jonjongames1997@users.noreply.github.com>
2024-11-02 18:54:02 -0400

    Update Settings.cs

--- Stuff/Settings.cs
+++ Stuff/Settings.cs
@@ -48,6 +48,7 @@ namespace JMCalloutsRemastered
         internal static bool DomesticDisturbanceHarmony = true;
         internal static bool DomesticDisturbanceSandyShores = true;
         internal static bool PublicOrdinanceEasterEgg1 = true;
+        internal static bool PublicOrdinanceEasterEgg2 = true;
         internal static bool HelpMessages = true;
         internal static bool WarningMessages = true;
         internal static Keys EndCall = Keys.End;
@@ -102,6 +103,7 @@ namespace JMCalloutsRemastered
             Settings.DomesticDisturbanceHarmony = ini.ReadBoolean("Callouts", "DomesticDisturbanceHarmony", true);
             Settings.DomesticDistrubancePaletoBay = ini.ReadBoolean("Callouts", "DomesticDistrubancePaletoBay", true);
             Settings.PublicOrdinanceEasterEgg1 = ini.ReadBoolean("Callouts", "PublicOrdinanceEasterEgg1", true);
+            Settings.PublicOrdinanceEasterEgg2 = ini.ReadBoolean("Callouts", "PublicOrdinanceEasterEgg2", true);
             Settings.HelpMessages = ini.ReadBoolean("HelpMessages", "HelpMessages", true);
             Settings.WarningMessages = ini.ReadBoolean("Misc", "WarningMessages", true);
 
