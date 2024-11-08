0ef71e7ab83b74cfc535105b5b150ac76f9e270c
jonjongames1997 <87885521+jonjongames1997@users.noreply.github.com>
2024-11-08 00:44:43 -0500

    Update PossibleProstitution.cs

--- Callouts/PossibleProstitution.cs
+++ Callouts/PossibleProstitution.cs
@@ -60,7 +60,15 @@ namespace JMCalloutsRemastered.Callouts
         {
             Game.LogTrivial("[JM Callouts Remastered Log]: Possible Prostitution callout accepted!");
             Game.DisplayNotification("web_jonjongames", "web_jonjongames", "~w~JM Callouts Remastered", "~w~Possible Prostitution", "~b~Dispatch:~w~ Suspect has been spotted. Respond ~r~Code 2.");
-            Game.DisplayHelp("Press ~y~END~w~ at anytime to end the callout", false);
+
+            if (Settings.HelpMessages)
+            {
+                Game.DisplayHelp("Press ~y~END~w~ at anytime to end the callout", false);
+            }
+            else
+            {
+                Settings.HelpMessages = false;
+            }
 
             LSPD_First_Response.Mod.API.Functions.PlayScannerAudioUsingPosition("JMCallouts_Respond_Code_2_Audio", Spawnpoint);
 
