873cfcab6784794e03d49fcc9a338853918d3927
jonjongames1997 <87885521+jonjongames1997@users.noreply.github.com>
2024-12-02 06:30:06 -0500

    Update TrespassingOnSchoolGrounds.cs

--- Callouts/TrespassingOnSchoolGrounds.cs
+++ Callouts/TrespassingOnSchoolGrounds.cs
@@ -71,9 +71,9 @@ namespace JMCalloutsRemastered.Callouts
         {
             if(MainPlayer.DistanceTo(suspect) <= 10f)
             {
-                Game.DisplayHelp("Press ~y~E~w~ to interact with ~r~Suspect~w~.", false);
+                Game.DisplayHelp("Press ~y~" + Settings.Dialog + "~w~ to interact with ~r~Suspect~w~.", false);
 
-                if (Game.IsKeyDown(System.Windows.Forms.Keys.E))
+                if (Game.IsKeyDown(Settings.Dialog))
                 {
                     counter++;
 
