c0e410bd9fb1978c35c3ef93e07fc7fad017e6d2
jonjongames1997 <87885521+jonjongames1997@users.noreply.github.com>
2024-12-02 06:29:49 -0500

    Update Soliciting.cs

--- Callouts/Soliciting.cs
+++ Callouts/Soliciting.cs
@@ -80,9 +80,9 @@ namespace JMCalloutsRemastered.Callouts
 
             if (MainPlayer.DistanceTo(Suspect) <= 10f)
             {
-                Game.DisplayHelp("Press ~y~E~w~ to talk to Suspect. ~y~Approach with caution~w~.", false);
+                Game.DisplayHelp("Press ~y~" + Settings.Dialog + "~w~ to talk to Suspect. ~y~Approach with caution~w~.", false);
 
-                if (Game.IsKeyDown(System.Windows.Forms.Keys.E))
+                if (Game.IsKeyDown(Settings.Dialog))
                 {
                     counter++;
 
