b7758410c51f58269fbddea2627e8b26d0d4c2da
jonjongames1997 <87885521+jonjongames1997@users.noreply.github.com>
2024-12-02 06:29:09 -0500

    Update HomelessPerson.cs

--- Callouts/HomelessPerson.cs
+++ Callouts/HomelessPerson.cs
@@ -77,9 +77,9 @@ namespace JMCalloutsRemastered.Callouts
             if(MainPlayer.DistanceTo(suspect) <= 5f)
             {
 
-                Game.DisplayHelp("Press ~y~E~w~ to speak with the ~r~suspect~w~.", false);
+                Game.DisplayHelp("Press ~y~" + Settings.Dialog + "~w~ to speak with the ~r~suspect~w~.", false);
 
-                if(Game.IsKeyDown(System.Windows.Forms.Keys.E))
+                if(Game.IsKeyDown(Settings.Dialog))
                 {
                     counter++;
 
